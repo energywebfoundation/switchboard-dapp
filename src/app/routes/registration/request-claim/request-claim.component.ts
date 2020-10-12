6a0e96a4dd0301c575854681e962160d067a62ff
Kim Honoridez <kim.energyweb@gmail.com>
2020-10-13 00:24:45 +0800

    Add template engine

--- src/app/routes/registration/request-claim/request-claim.component.ts
+++ src/app/routes/registration/request-claim/request-claim.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-request-claim',
   templateUrl: './request-claim.component.html',
@@ -6,9 +7,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RequestClaimComponent implements OnInit {
 
-  constructor() { }
+  public enrolmentForm: FormGroup;
+  public fieldList: {type: string, label: string, validation: string}[];
+  
+  constructor(private fb: FormBuilder) { 
+    // Get data here
+    this.fieldList = [{
+      type: 'text',
+      label: 'Name',
+      validation: ''
+    },{
+      type: 'number',
+      label: 'Age',
+      validation: ''
+    },{
+      type: 'date',
+      label: 'Birthdate',
+      validation: ''
+    },{
+      type: 'boolean',
+      label: 'Married',
+      validation: ''
+    }];
+
+    let controls = [];
+    for (let { type, label, validation} of this.fieldList) {
+      let control = new FormControl();
+      switch (type) {
+        case 'text':
+          break;
+        case 'number':
+          break;
+        case 'date':
+          break;
+        case 'boolean':
+          break;
+      }
+
+      // add validations
+
+      // add control to array
+      controls.push(control);
+    }
+
+    this.enrolmentForm = fb.group({
+      fields: fb.array(controls)
+    });
+
+    console.log(this.enrolmentForm);
+  }
 
   ngOnInit() {
+    
   }
 
 }
