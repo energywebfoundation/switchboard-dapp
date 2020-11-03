009d4317a5a6e173be5169e4d7e4ca148722fc8c
Kim Honoridez <kim.energyweb@gmail.com>
2020-11-03 23:23:42 +0800

    Allow Update Application

--- src/app/routes/applications/new-application/new-application.component.ts
+++ src/app/routes/applications/new-application/new-application.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialog, MatDialogRef, MatStepper } from '@angular/material';
+import { MatDialog, MatDialogRef, MatStepper, MAT_DIALOG_DATA } from '@angular/material';
 import { ENSNamespaceTypes } from 'iam-client-lib';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { IamService } from 'src/app/shared/services/iam.service';
 import { environment } from 'src/environments/environment';
 import { ConfirmationDialogComponent } from '../../widgets/confirmation-dialog/confirmation-dialog.component';
-
-const TOASTR_HEADER = 'Create New Application';
+import { ViewType } from '../new-organization/new-organization.component';
 
 @Component({
   selector: 'app-new-application',
@@ -22,13 +21,20 @@ export class NewApplicationComponent implements OnInit {
   public environment = environment;
   public isChecking = false;
   public ENSPrefixes = ENSNamespaceTypes;
+  public ViewType = ViewType;
+
+  viewType: string;
+  origData: any;
+
+  private TOASTR_HEADER = 'Create New Application';
   
   constructor(private fb: FormBuilder,
     private iamService: IamService,
     private toastr: ToastrService,
     private spinner: NgxSpinnerService,
     public dialogRef: MatDialogRef<NewApplicationComponent>,
-    public dialog: MatDialog) { 
+    public dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public data: any) { 
       this.appForm = fb.group({
         orgNamespace: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(256)])],
         appName: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(256)])],
@@ -41,9 +47,55 @@ export class NewApplicationComponent implements OnInit {
           others: ''
         })
       });
+
+      if (data && data.viewType && data.origData) {
+        console.log('origData', this.origData);
+        this.viewType = data.viewType;
+        this.origData = data.origData;
+  
+        if (this.viewType === ViewType.UPDATE) {
+          this.TOASTR_HEADER = 'Update Application';
+        }
+  
+        this.initFormData();
+      }
     }
 
   ngOnInit() {
+  }
+
+  private initFormData() {
+    if (this.origData) {
+      let def = this.origData.definition;
+      let others = undefined;
+
+      // Construct Others
+      if (def.others) {
+        let tmp: any[] = def.others;
+        others = {};
+
+        for (let item of tmp) {
+          others[item.key] = item.value;
+        }
+
+        others = JSON.stringify(others);
+      }
+
+      // Construct Organization
+      let arr = this.origData.namespace.split(ENSNamespaceTypes.Application);
+
+      this.appForm.patchValue({
+        orgNamespace: arr[1].substring(1),
+        appName: this.origData.name,
+        data: {
+          applicationName: def.appName,
+          logoUrl: def.logoUrl,
+          websiteUrl: def.websiteUrl,
+          description: def.description,
+          others: others
+        }
+      });
+    }
   }
 
   alphaNumericOnly(event: any, includeDot?: boolean) {
@@ -86,15 +138,15 @@ export class NewApplicationComponent implements OnInit {
               this.stepper.next();
             }
             else {
-              this.toastr.error('You are not authorized to create an application in this organization.', TOASTR_HEADER);
+              this.toastr.error('You are not authorized to create an application in this organization.', this.TOASTR_HEADER);
             }
           }
           else {
-            this.toastr.error('Application subdomain in this organization does not exist.', TOASTR_HEADER);
+            this.toastr.error('Application subdomain in this organization does not exist.', this.TOASTR_HEADER);
           }
         }
         else {
-          this.toastr.error('Organization namespace does not exist.', TOASTR_HEADER);
+          this.toastr.error('Organization namespace does not exist.', this.TOASTR_HEADER);
         }
       }
       catch (e) {
@@ -145,7 +197,7 @@ export class NewApplicationComponent implements OnInit {
           allowToProceed = false;
 
           // Do not allow to proceed if app namespace already exists
-          this.toastr.error('Application namespace already exists. You have no access rights to it.', TOASTR_HEADER);
+          this.toastr.error('Application namespace already exists. You have no access rights to it.', this.TOASTR_HEADER);
         }
         else {
           this.spinner.hide();
@@ -179,13 +231,76 @@ export class NewApplicationComponent implements OnInit {
           }
           catch (e) {
             console.error(orgData.data.others, e);
-            this.toastr.error('Others must be in JSON format.', TOASTR_HEADER);
+            this.toastr.error('Others must be in JSON format.', this.TOASTR_HEADER);
           }
         }
       }
     }
     else {
-      this.toastr.error('Form is invalid.', TOASTR_HEADER);
+      this.toastr.error('Form is invalid.', this.TOASTR_HEADER);
+    }
+
+    this.isChecking = false;
+    this.spinner.hide();
+  }
+
+  async updateApp() {
+    this.spinner.show();
+    this.isChecking = true;
+
+    if (this.appForm.valid) {
+      let allowToProceed = true;
+      let orgData = this.appForm.value;
+
+      // Check if current user is the owner of this namespace and allow him/her to overwrite
+      let isOwner = await this.iamService.iam.isOwner({
+        domain: `${orgData.appName}.${this.ENSPrefixes.Application}.${orgData.orgNamespace}`
+      });
+
+      if (!isOwner) {
+        allowToProceed = false;
+
+        // Do not allow to proceed if app namespace already exists
+        this.toastr.error('You have no update rights to this namespace.', this.TOASTR_HEADER);
+      }
+      else {
+        this.spinner.hide();
+        
+        // Prompt if user wants to overwrite this namespace
+        if (!await this.confirm('You are updating details of this application. Do you wish to continue?')) {
+          allowToProceed = false;
+        }
+        else {
+          this.spinner.show();
+        }
+      }
+
+      if (allowToProceed) {
+        if (!orgData.data.others || !orgData.data.others.trim()) {
+          // Let the user confirm the info before proceeding to the next step
+          this.stepper.selected.editable = false;
+          this.stepper.selected.completed = true;
+          this.stepper.next();
+        }
+        else {
+          try {
+            // Check if others is in JSON Format
+            console.info(JSON.parse(orgData.data.others));
+
+            // Let the user confirm the info before proceeding to the next step
+            this.stepper.selected.editable = false;
+            this.stepper.selected.completed = true;
+            this.stepper.next();
+          }
+          catch (e) {
+            console.error(orgData.data.others, e);
+            this.toastr.error('Others must be in JSON format.', this.TOASTR_HEADER);
+          }
+        }
+      }
+    }
+    else {
+      this.toastr.error('Form is invalid.', this.TOASTR_HEADER);
     }
 
     this.isChecking = false;
@@ -207,7 +322,7 @@ export class NewApplicationComponent implements OnInit {
         req.data.others = JSON.parse(req.data.others);
       }
       catch (e) {
-        this.toastr.error('Others must be in JSON format.', TOASTR_HEADER);
+        this.toastr.error('Others must be in JSON format.', this.TOASTR_HEADER);
         return;
       }
     }
@@ -221,6 +336,15 @@ export class NewApplicationComponent implements OnInit {
     let list = this.stepper.steps.toArray();
     list[1].editable = false;
 
+    if (this.viewType === ViewType.UPDATE) {
+      this.proceedUpdateStep(req);
+    }
+    else {
+      this.proceedCreateSteps(req);
+    }
+  }
+
+  private async proceedCreateSteps(req: any) {
     try {
       // Retrieve the steps to create an application
       let steps = await this.iamService.iam.createApplication(req);
@@ -247,12 +371,36 @@ export class NewApplicationComponent implements OnInit {
     }
   }
 
+  private async proceedUpdateStep(req: any) {
+    try {
+      // Update steps
+      this.stepper.selected.completed = true;
+      this.stepper.next();
+
+      // Set Definition
+      const newDomain = `${req.appName}.${req.namespace}`;
+      await this.iamService.iam.setRoleDefinition({
+        data: req.data,
+        domain: newDomain
+      });
+
+      // Move to Complete Step
+      this.toastr.info('Set definition for application', 'Transaction Success');
+      this.stepper.selected.completed = true;
+      this.stepper.next();
+    }
+    catch (e) {
+      console.error('Update App Error', e);
+      this.toastr.error(e.message || 'Please contact system administrator.', 'System Error');
+    }
+  }
+
   private async confirm(confirmationMsg: string) {
     return this.dialog.open(ConfirmationDialogComponent, {
       width: '400px',
       maxHeight: '180px',
       data: {
-        header: TOASTR_HEADER,
+        header: this.TOASTR_HEADER,
         message: confirmationMsg
       },
       maxWidth: '100%',
@@ -268,7 +416,7 @@ export class NewApplicationComponent implements OnInit {
     }
     else {
       if (isSuccess) {
-        this.toastr.success('Application is successfully created.', TOASTR_HEADER);
+        this.toastr.success('Application is successfully created.', this.TOASTR_HEADER);
       }
       this.dialogRef.close(isSuccess);
     }
