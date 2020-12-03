e783cbde71f965d2f6547ce1910417c79c47dfb0
Mani H <mani.hagh.sefat@energyweb.org>
2020-12-03 19:00:04 +0800

    Update timeout and fix display label

--- src/app/shared/services/iam.service.ts
+++ src/app/shared/services/iam.service.ts
@@ -213,15 +213,15 @@ export class IamService {
 
   public waitForSignature(isConnectAndSign?: boolean) {
     this._throwTimeoutError = false;
-    let timeout = 20000;
+    let timeout = 60000;
     let messageType = 'sign';
     if (isConnectAndSign) {
       messageType = 'connect to your wallet and sign';
     }
 
-    this.loadingService.show(['Your signature is being requested.', `Please ${messageType} within ${timeout / 2000} seconds or you will be automatically logged-out.`]);
+    this.loadingService.show(['Your signature is being requested.', `Please ${messageType} within ${timeout / 1000} seconds or you will be automatically logged-out.`]);
     this._timer = setTimeout(() => {
-      this._displayTimeout(timeout / 2000, isConnectAndSign);
+      this._displayTimeout(timeout / 1000, isConnectAndSign);
       this.clearWaitSignatureTimer();
       this._throwTimeoutError = true;
     }, timeout);
