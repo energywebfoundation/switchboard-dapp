52a483aace5f8607a3cd88174d9e6e622da1c852
Daniel Wojno <daniel.wojno@tsh.io>
2021-02-26 14:19:20 +0100

    feat(sentry): add error loggging to sentry

--- src/environments/constants.template.ts
+++ src/environments/constants.template.ts
@@ -3,7 +3,7 @@ export const constants = {
   orgNamespace: 'iam.ewc',
   trxRetry: 60, // 1 minute
   userIdle: 900, // 15 mins in seconds
-  SENTRY_DNS: '',
-  SENTRY_RELEASE: '',
-  SENTRY_ENVIRONMENT: '',
+  SENTRY_DNS: '${SENTRY_DNS}',
+  SENTRY_RELEASE: '$SENTRY_RELEASE',
+  SENTRY_ENVIRONMENT: '${SENTRY_ENVIRONMENT}',
 }
