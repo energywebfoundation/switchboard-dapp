cf189d801d1fc43541f5b826d42c5c2fd031f80d
Dawid Gil <dawid.gil@j-labs.pl>
2021-05-12 11:15:53 +0200

    fix header and layout components spec files.

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,22 +1,46 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async, inject } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { UserIdleService } from 'angular-user-idle';
 import { HeaderComponent } from './header.component';
-
+import { MenuService } from '../../core/menu/menu.service';
+import { IamService } from '../../shared/services/iam.service';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NotificationService } from '../../shared/services/notification.service';
 import { UserblockService } from '../sidebar/userblock/userblock.service';
 import { SettingsService } from '../../core/settings/settings.service';
-import { MenuService } from '../../core/menu/menu.service';
+import { MatDialog } from '@angular/material/dialog';
+import { DomSanitizer } from '@angular/platform-browser';
 
-describe('Component: Header', () => {
+describe('HeaderComponent', () => {
+    let component: HeaderComponent;
+    let fixture: ComponentFixture<HeaderComponent>;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [MenuService, UserblockService, SettingsService]
-        }).compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [HeaderComponent],
+            providers: [
+                {provide: UserIdleService, useClass: {}},
+                {provide: MenuService, useClass: {}},
+                {provide: IamService, useClass: {}},
+                {provide: Router, useClass: {}},
+                {provide: ToastrService, useClass: {}},
+                {provide: NotificationService, useClass: {}},
+                {provide: UserblockService, useClass: {}},
+                {provide: SettingsService, useClass: {}},
+                {provide: MatDialog, useClass: {}},
+                {provide: DomSanitizer, useClass: {}},
+            ]
+        })
+          .compileComponents();
     });
 
-    it('should create an instance', async(inject([MenuService, UserblockService, SettingsService], (menuService, userblockService, settingsService) => {
-        let component = new HeaderComponent(menuService, userblockService, settingsService); 
+    beforeEach(() => {
+        fixture = TestBed.createComponent(HeaderComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
         expect(component).toBeTruthy();
-    })));
+    });
 });
--- src/app/layout/layout.component.spec.ts
+++ src/app/layout/layout.component.spec.ts
@@ -1,11 +1,30 @@
-/* tslint:disable:no-unused-variable */
-
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LayoutComponent } from './layout.component';
+import { UserIdleService } from 'angular-user-idle';
+import { IamService } from '../shared/services/iam.service';
 
-describe('Component: Layout', () => {
-  it('should create an instance', () => {
-    let component = new LayoutComponent();
+describe('LayoutComponent', () => {
+  let component: LayoutComponent;
+  let fixture: ComponentFixture<LayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LayoutComponent],
+      providers: [
+        {provide: UserIdleService, useClass: {}},
+        {provide: IamService, useClass: {}}
+      ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
