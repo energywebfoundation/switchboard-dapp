7c6785dccafbc699109f908cf9f4c7ac3c83b642
Dawid Gil <dawid.gil@j-labs.pl>
2021-05-12 10:44:30 +0200

    remove errors from minified-did-viewer directive and retry btn directive spec files

--- src/app/shared/directives/minified-did-viewer/minified-did-viewer.directive.spec.ts
+++ src/app/shared/directives/minified-did-viewer/minified-did-viewer.directive.spec.ts
@@ -1,8 +1,31 @@
 import { MinifiedDidViewerDirective } from './minified-did-viewer.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  template: '<p [appMinifiedDidViewer]="appMinifiedDidViewer">Testing Directives is awesome!</p>'
+})
+class TestComponent {
+  @Input() appMinifiedDidViewer = false;
+}
 
 describe('MinifiedDidViewerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MinifiedDidViewerDirective();
-    expect(directive).toBeTruthy();
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        MinifiedDidViewerDirective
+      ]
+    });
+
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create component', () => {
+    expect(component).toBeDefined();
   });
 });
