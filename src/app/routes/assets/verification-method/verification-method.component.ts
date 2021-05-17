b643edec3fef885a27bb7b536a71eda45fd1055b
Davy Castillo <davycastillo9@gmail.com>
2021-05-17 18:13:22 +0800

    add html for verification method

---
+++ src/app/routes/assets/verification-method/verification-method.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-method',
+  templateUrl: './verification-method.component.html',
+  styleUrls: ['./verification-method.component.scss']
+})
+export class VerificationMethodComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
