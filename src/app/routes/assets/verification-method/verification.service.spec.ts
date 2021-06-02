773d8e416e42a3fc3f1c6e6313bf662373165581
Dawid Gil <dawid.gil@j-labs.pl>
2021-06-02 11:48:57 +0200

    add few tests for verification service.

--- src/app/routes/assets/verification-method/verification.service.spec.ts
+++ src/app/routes/assets/verification-method/verification.service.spec.ts
@@ -7,14 +7,14 @@ import { SwitchboardToastrService } from '../../../shared/services/switchboard-t
 
 describe('VerificationService', () => {
   let service: VerificationService;
-  const iamServiceSpy = jasmine.createSpyObj('IamService', ['iam']);
+  const iamSpy = jasmine.createSpyObj('iam', ['updateDidDocument', 'getDidDocument']);
   const loadingServiceSpy = jasmine.createSpyObj('LoadingService', ['show', 'hide']);
   const toastrSpy = jasmine.createSpyObj('ToastrService', ['success']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
-        { provide: IamService, useValue: iamServiceSpy },
+        { provide: IamService, useValue: { iam: iamSpy } },
         { provide: LoadingService, useValue: loadingServiceSpy },
         { provide: SwitchboardToastrService, useValue: toastrSpy }
       ]
@@ -24,5 +24,36 @@ describe('VerificationService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('updateDocumentAndReload', () => {
+    it('should check returned value', () => {
+      const publicKey = {
+        controller: '0xAB6b0b9f9002B2a962D5Fe7a0F2A3402C4A8FaFC',
+        id: 'did:ethr:0xAB6b0b9f9002B2a962D5Fe7a0F2A3402C4A8FaFC#key-owner',
+        publicKeyHex: '0x0235c8f279e7c9d63e1f7ed4084ed8ceb7e1f8877b31507ab73019a5b90ca43ff1',
+        type: 'Secp256k1veriKey'
+      };
+      iamSpy.updateDidDocument.and.returnValue(Promise.resolve());
+      iamSpy.getDidDocument.and.returnValue(Promise.resolve({
+        publicKey: [publicKey]
+      }));
+      service.updateDocumentAndReload('', '').subscribe((publicKeys) => {
+        expect(publicKeys.length).toBe(1);
+        expect(publicKeys).toContain(jasmine.objectContaining(publicKey));
+      });
+      expect(loadingServiceSpy.show).toHaveBeenCalled();
+    });
+
+    it('should check while error occurs', () => {
+      iamSpy.updateDidDocument.and.returnValue(Promise.reject());
+
+      service.updateDocumentAndReload('', '').subscribe(() => {
+      }, () => {
+        expect(toastrSpy.error).toHaveBeenCalled();
+      });
+      expect(loadingServiceSpy.show).toHaveBeenCalled();
+    });
+
   });
 });
