4d8e85cda329b79d0c2f9b146421eded2430ec39
Dawid Gil <dawid.gil@j-labs.pl>
2021-06-05 12:00:22 +0200

    update form with data from store.

--- src/app/layout/header/dialog-user/dialog-user.component.ts
+++ src/app/layout/header/dialog-user/dialog-user.component.ts
@@ -1,102 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
 import { IamService } from 'src/app/shared/services/iam.service';
 import { LoadingService } from 'src/app/shared/services/loading.service';
 import { CancelButton } from '../../loading/loading.component';
+import { Store } from '@ngrx/store';
+import * as userSelectors from '../../../state/user-claim/user.selectors';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
-    selector: 'dialog-user',
-    templateUrl: 'dialog-user.component.html',
-    styleUrls: ['../header.component.scss']
+  selector: 'dialog-user',
+  templateUrl: 'dialog-user.component.html',
+  styleUrls: ['../header.component.scss']
 })
-export class DialogUser implements OnInit {
+export class DialogUser implements OnInit, OnDestroy {
 
-    public profileForm      : FormGroup;
-    public maxDate          : Date;
-    private profileCache: any;
+  public profileForm: FormGroup;
+  public maxDate: Date;
+  private profileCache: any;
+  private destroy$ = new Subject<void>();
 
-    constructor(
-        public dialogRef: MatDialogRef<DialogUser>,
-        private fb: FormBuilder,
-        private iamService: IamService,
-        private toastr: ToastrService,
-        private loadingService: LoadingService) {
-            this.profileForm = fb.group({
-                name: ['', Validators.compose([
-                    Validators.maxLength(256),
-                    Validators.required
-                ])],
-                birthdate: ['', Validators.required],
-                address: ['', Validators.compose([
-                    Validators.maxLength(500),
-                    Validators.required
-                ])]
-            });
-            
-            let today = new Date();
-            today.setFullYear(today.getFullYear() - 18);
-            this.maxDate = today;    
-    }
+  constructor(
+    public dialogRef: MatDialogRef<DialogUser>,
+    private fb: FormBuilder,
+    private iamService: IamService,
+    private toastr: ToastrService,
+    private loadingService: LoadingService,
+    private store: Store) {
+    this.profileForm = fb.group({
+      name: ['', Validators.compose([
+        Validators.maxLength(256),
+        Validators.required
+      ])],
+      birthdate: ['', Validators.required],
+      address: ['', Validators.compose([
+        Validators.maxLength(500),
+        Validators.required
+      ])]
+    });
 
-    async ngOnInit() {
-        this.loadingService.show();
+    const today = new Date();
+    today.setFullYear(today.getFullYear() - 18);
+    this.maxDate = today;
+  }
 
-        // Get User Claims
-        let data: any[] = await this.iamService.iam.getUserClaims();
-        // console.log('getUserClaims()', JSON.parse(JSON.stringify(data)));
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
-        // Get Profile Related Claims
-        data = data.filter((item: any) => item.profile ? true : false );
-        // console.log('Profile Claims', JSON.parse(JSON.stringify(data)));
+  ngOnInit() {
+    this.store.select(userSelectors.getUserProfile)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((profile => this.profileForm.patchValue({
+        name: profile.name,
+        birthdate: new Date(profile.birthdate),
+        address: profile.address
+      })));
 
-        // Get the most recent claim
-        if (data.length) {
-            this.profileCache = data[0].profile;
-            let tmp: any = data[0].profile;
-            this.profileForm.patchValue({
-                name: tmp.name,
-                birthdate: new Date(tmp.birthdate),
-                address: tmp.address
-            });
-        }
+  }
 
+  async save() {
+    if (this.profileForm.valid) {
+      this.loadingService.show('Please confirm this transaction in your connected wallet.', CancelButton.ENABLED);
+
+      let data = this.profileForm.getRawValue();
+
+      if (data.birthdate) {
+        const date = data.birthdate.getTime();
+        data = JSON.parse(JSON.stringify(data));
+        data.birthdate = date;
+      }
+
+      // console.log('data', data);
+      try {
+        await this.iamService.iam.createSelfSignedClaim({
+          data: {
+            profile: {
+              ...this.profileCache,
+              ...data
+            }
+          }
+        });
+        this.toastr.success('Identity is updated.', 'Success');
+        this.dialogRef.close(true);
+      } catch (e) {
+        console.error('Saving Identity Error', e);
+        this.toastr.error(e.message, 'System Error');
+      } finally {
         this.loadingService.hide();
+      }
     }
-
-    async save() {
-        if (this.profileForm.valid) {
-            this.loadingService.show('Please confirm this transaction in your connected wallet.', CancelButton.ENABLED);
-
-            let data = this.profileForm.getRawValue();
-
-            if (data.birthdate) {
-                let date = data.birthdate.getTime();
-                data = JSON.parse(JSON.stringify(data));
-                data.birthdate = date;    
-            }
-            
-            // console.log('data', data);
-            try {
-                await this.iamService.iam.createSelfSignedClaim({
-                    data: {
-                        profile: {
-                            ...this.profileCache,
-                            ...data
-                        }
-                    }
-                });
-                this.toastr.success('Identity is updated.', 'Success');
-                this.dialogRef.close(true);
-            }
-            catch (e) {
-                console.error('Saving Identity Error', e);
-                this.toastr.error(e.message, 'System Error')
-            }
-            finally {
-                this.loadingService.hide();
-            }
-        }
-    }
+  }
 }
