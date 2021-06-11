4375fb5c09177996c656592db9a3aa0253df6e7e
dawidgil <dawid.gil@j-labs.pl>
2021-06-11 14:52:49 +0200

    allow user to pass only alphanumeric characters when setting roleName.

---
+++ src/app/utils/validators/is-alphanumeric.validator.ts
@@ -0,0 +1,16 @@
+import { AbstractControl } from '@angular/forms';
+
+const alphaNumericRegex = new RegExp(/^[a-z0-9]+$/i);
+
+export function isAlphanumericValidator(control: AbstractControl) {
+  if (!control.value) {
+    return null;
+  }
+  if (alphaNumericRegex.test(control.value)) {
+    return null;
+  }
+  return {
+    isAlphaNumericInvalid: true
+  };
+
+}
