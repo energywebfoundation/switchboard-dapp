988d469cff82a08be6055deceeae99d845753eac
dawidgil <dawid.gil@j-labs.pl>
2021-06-18 12:15:08 +0200

    add providers component to be displayed when navigating to stake view.

--- src/app/routes/stake/stake-routing.module.ts
+++ src/app/routes/stake/stake-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { StakeComponent } from './stake.component';
+import { ProvidersComponent } from './providers/providers.component';
 
 const routes = [
   {
     path: '',
-    component: StakeComponent
+    component: StakeComponent,
+    children: [
+      {path: '', component: ProvidersComponent},
+    ]
   }
 ];
 
--- src/app/routes/stake/stake.module.ts
+++ src/app/routes/stake/stake.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StakeComponent } from './stake.component';
 import { StakeRoutingModule } from './stake-routing.module';
+import { ProvidersComponent } from './providers/providers.component';
 
 
 @NgModule({
-  declarations: [StakeComponent],
+  declarations: [StakeComponent, ProvidersComponent],
   imports: [
     CommonModule,
     StakeRoutingModule
