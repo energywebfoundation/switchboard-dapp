a14eee619fe449fb5a7fd785ab0653fd811bc3e9
dawidgil <dawid.gil@j-labs.pl>
2021-06-19 22:49:14 +0200

    fix running enrolment-form tests.

--- src/app/routes/registration/enrolment-form/enrolment-form.component.spec.ts
+++ src/app/routes/registration/enrolment-form/enrolment-form.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EnrolmentFormComponent } from './enrolment-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 describe('EnrolmentFormComponent', () => {
   let component: EnrolmentFormComponent;
@@ -8,7 +13,14 @@ describe('EnrolmentFormComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EnrolmentFormComponent ]
+      declarations: [ EnrolmentFormComponent ],
+      imports: [
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCheckboxModule
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +28,11 @@ describe('EnrolmentFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(EnrolmentFormComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    component.fieldList = [];
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
