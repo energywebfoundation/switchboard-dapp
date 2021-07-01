25bcfede564dfb17b30855455cf465b28db85cb1
dawidgil <dawid.gil@j-labs.pl>
2021-07-01 13:00:27 +0200

    add error messages that fields are required.

--- src/app/layout/header/dialog-user/dialog-user.component.ts
+++ src/app/layout/header/dialog-user/dialog-user.component.ts
@@ -7,6 +7,7 @@ import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import * as userActions from '../../../state/user-claim/user.actions';
 import { UserClaimState } from '../../../state/user-claim/user.reducer';
+import { Profile } from 'iam-client-lib';
 
 const MAJORITY_AGE = 18;
 
@@ -18,15 +19,15 @@ const MAJORITY_AGE = 18;
 export class DialogUserComponent implements OnInit, OnDestroy {
 
   public profileForm: FormGroup = this.fb.group({
-    name: ['', Validators.compose([
+    name: ['', [
       Validators.maxLength(256),
       Validators.required
-    ])],
+    ]],
     birthdate: ['', Validators.required],
-    address: ['', Validators.compose([
+    address: ['', [
       Validators.maxLength(500),
       Validators.required
-    ])]
+    ]]
   });
   public maxDate: Date;
   private destroy$ = new Subject<void>();
@@ -46,26 +47,26 @@ export class DialogUserComponent implements OnInit, OnDestroy {
     this.maxDate = this.getLegalAge();
     this.store.select(userSelectors.getUserProfile)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((profile => this.profileForm.patchValue({
-        name: profile.name,
-        birthdate: new Date(profile.birthdate),
-        address: profile.address
-      })));
-
+      .subscribe(profile => this.profileForm.patchValue({
+        name: profile?.name,
+        birthdate: new Date(profile?.birthdate),
+        address: profile?.address
+      }));
   }
 
-  async save() {
-    if (this.profileForm.valid) {
-      let data = this.profileForm.getRawValue();
-
-      if (data.birthdate) {
-        const date = data.birthdate.getTime();
-        data = JSON.parse(JSON.stringify(data));
-        data.birthdate = date;
-      }
-
-      this.store.dispatch(userActions.updateUserClaims({profile: data}));
+  save() {
+    if (this.profileForm.invalid) {
+      return;
     }
+    this.store.dispatch(userActions.updateUserClaims({profile: this.getProfile()}));
+  }
+
+  private getProfile(): Partial<Profile> {
+    const profileData = this.profileForm.getRawValue();
+    return {
+      ...profileData,
+      birthdate: profileData.birthdate.getTime()
+    };
   }
 
   private getLegalAge() {
