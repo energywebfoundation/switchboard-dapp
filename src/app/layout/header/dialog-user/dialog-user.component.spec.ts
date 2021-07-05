dde7402c902b168a77294440dd6646564c47b708
DawidGil-jlabs <83639710+DawidGil-jlabs@users.noreply.github.com>
2021-07-05 09:42:48 +0200

    SWTCH-1101 missing error messages in dialog form for updating user data.
    * add error messages that fields are required.
    
    * remove dialog ref from constructor.
    
    * add tests for dialog user component.
    
    * add tests and partially refactor dialog-user component
    
    * fix test.

---
+++ src/app/layout/header/dialog-user/dialog-user.component.spec.ts
@@ -0,0 +1,122 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { DialogUserComponent } from './dialog-user.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { UserClaimState } from '../../../state/user-claim/user.reducer';
+import * as userSelectors from '../../../state/user-claim/user.selectors';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+
+describe('DialogUserComponent', () => {
+  let component: DialogUserComponent;
+  let fixture: ComponentFixture<DialogUserComponent>;
+  let hostDebug: DebugElement;
+  let store: MockStore<UserClaimState>;
+
+  const fieldSelector = (id, postSelector = '') => hostDebug.query(By.css(`[data-qa-id=field-${id}] ${postSelector}`));
+  const dispatchEvent = (el) => {
+    el.dispatchEvent(new Event('input'));
+    el.dispatchEvent(new Event('blur'));
+  };
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [DialogUserComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        NoopAnimationsModule,
+        MatInputModule,
+      ],
+      providers: [
+        provideMockStore()
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    })
+      .compileComponents();
+    store = TestBed.inject(MockStore);
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DialogUserComponent);
+    component = fixture.componentInstance;
+    hostDebug = fixture.debugElement;
+  });
+
+  it('should create', () => {
+    store.overrideSelector(userSelectors.getUserProfile, {});
+    expect(component).toBeTruthy();
+  });
+
+  it('should update form with values from store', () => {
+    const date = new Date('2001-06-12');
+    const data = {
+      name: 'name',
+      address: '123',
+      birthdate: date.getTime()
+    };
+    store.overrideSelector(userSelectors.getUserProfile, data as any);
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const {name, birthdate, address} = setup(hostDebug);
+
+    expect(name.value).toBe(data.name);
+    expect(address.value).toBe(data.address);
+    expect(new Date(birthdate.value)).toEqual(date);
+  });
+
+  it('should dispatch updateUserClaims action when submitting', () => {
+    const {name, birthdate, address, submit} = setup(hostDebug);
+    const dispatchSpy = spyOn(store, 'dispatch');
+    component.ngOnInit();
+    fixture.detectChanges();
+    name.value = 'name';
+    dispatchEvent(name);
+
+    birthdate.value = '1/1/2000';
+    dispatchEvent(birthdate);
+
+    address.value = 'Some address';
+    dispatchEvent(address);
+    fixture.detectChanges();
+
+    expect(submit.disabled).toBeFalsy();
+
+    submit.click();
+    expect(dispatchSpy).toHaveBeenCalled();
+  });
+
+  it('should display error message for maximum allowed date', () => {
+    const {birthdate} = setup(hostDebug);
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    birthdate.value = todayDate();
+    dispatchEvent(birthdate);
+
+    fixture.detectChanges();
+    const matError = hostDebug.query(By.css('mat-error')).nativeElement;
+
+    expect(matError.textContent).toContain('Maximum allowed date is');
+  });
+});
+
+const setup = (hostDebug: DebugElement) => {
+  return {
+    submit: hostDebug.query(By.css('[data-qa-id=submit]')).nativeElement,
+    name: hostDebug.query(By.css('[data-qa-id=name]')).nativeElement,
+    birthdate: hostDebug.query(By.css('[data-qa-id=birthdate]')).nativeElement,
+    address: hostDebug.query(By.css('[data-qa-id=address]')).nativeElement,
+  };
+};
+
+const todayDate = () => {
+  const date = new Date(Date.now());
+  return `${date.getDay()}/${date.getMonth()}/${date.getFullYear()}`;
+};
--- src/app/layout/header/dialog-user/dialog-user.component.ts
+++ src/app/layout/header/dialog-user/dialog-user.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
 import { Store } from '@ngrx/store';
 import * as userSelectors from '../../../state/user-claim/user.selectors';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import * as userActions from '../../../state/user-claim/user.actions';
 import { UserClaimState } from '../../../state/user-claim/user.reducer';
+import { Profile } from 'iam-client-lib';
 
 const MAJORITY_AGE = 18;
 
@@ -18,21 +18,20 @@ const MAJORITY_AGE = 18;
 export class DialogUserComponent implements OnInit, OnDestroy {
 
   public profileForm: FormGroup = this.fb.group({
-    name: ['', Validators.compose([
+    name: ['', [
       Validators.maxLength(256),
       Validators.required
-    ])],
+    ]],
     birthdate: ['', Validators.required],
-    address: ['', Validators.compose([
+    address: ['', [
       Validators.maxLength(500),
       Validators.required
-    ])]
+    ]]
   });
   public maxDate: Date;
   private destroy$ = new Subject<void>();
 
   constructor(
-    public dialogRef: MatDialogRef<DialogUserComponent>,
     private fb: FormBuilder,
     private store: Store<UserClaimState>) {
   }
@@ -46,26 +45,30 @@ export class DialogUserComponent implements OnInit, OnDestroy {
     this.maxDate = this.getLegalAge();
     this.store.select(userSelectors.getUserProfile)
       .pipe(takeUntil(this.destroy$))
-      .subscribe((profile => this.profileForm.patchValue({
-        name: profile.name,
-        birthdate: new Date(profile.birthdate),
-        address: profile.address
-      })));
-
+      .subscribe(profile => this.profileForm.patchValue({
+        name: profile?.name,
+        birthdate: new Date(profile?.birthdate),
+        address: profile?.address
+      }));
   }
 
-  async save() {
-    if (this.profileForm.valid) {
-      let data = this.profileForm.getRawValue();
+  disableSubmit(): boolean {
+    return this.profileForm.pristine || this.profileForm.invalid;
+  }
 
-      if (data.birthdate) {
-        const date = data.birthdate.getTime();
-        data = JSON.parse(JSON.stringify(data));
-        data.birthdate = date;
-      }
-
-      this.store.dispatch(userActions.updateUserClaims({profile: data}));
+  save() {
+    if (this.profileForm.invalid) {
+      return;
     }
+    this.store.dispatch(userActions.updateUserClaims({profile: this.getProfile()}));
+  }
+
+  private getProfile(): Partial<Profile> {
+    const profileData = this.profileForm.getRawValue();
+    return {
+      ...profileData,
+      birthdate: profileData.birthdate.getTime()
+    };
   }
 
   private getLegalAge() {
