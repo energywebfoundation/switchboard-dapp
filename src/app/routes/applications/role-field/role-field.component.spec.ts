137d25377759d9fd4b3878079260e00115635dfb
amaugosomto <amaugosomto@gmail.com>
2021-07-05 13:27:09 +0100

    added test cases for role-field

--- src/app/routes/applications/role-field/role-field.component.spec.ts
+++ src/app/routes/applications/role-field/role-field.component.spec.ts
@@ -1,10 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 import { RoleFieldComponent } from './role-field.component';
 
-describe('RoleFieldComponent', () => {
+fdescribe('RoleFieldComponent', () => {
   let component: RoleFieldComponent;
   let fixture: ComponentFixture<RoleFieldComponent>;
+  let hostDebug: DebugElement;
+  let host: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -16,10 +20,34 @@ describe('RoleFieldComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RoleFieldComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostDebug = fixture.debugElement;
+    host = hostDebug.nativeElement;
+    // fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('add field button', () => {
+    const { showField, addField } = setup(hostDebug);
+    showField.nativeElement.click();
+    fixture.detectChanges();
+
+    it('should open form to add field', () => {
+      expect(component.showAddFieldForm).toBeTruthy();
+    });
+
+    it('add button should be disabled', () => {
+      expect(addField.nativeElement.disabled).toBeTruthy();
+    });
+  })
+
+  const setup = (hostDebug: DebugElement) => {
+    return {
+      showField: hostDebug.query(By.css('[data-qa-id=show-field]')),
+      addField: hostDebug.query(By.css('[data-qa-id=add-field]')),
+    };
+  };
 });
+
