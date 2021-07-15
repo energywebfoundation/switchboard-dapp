460968da6356198f3f90cd85d168029f0444d5b1
dawidgil <dawid.gil@j-labs.pl>
2021-07-15 22:36:11 +0200

    fix ewt-patron component tests.

--- src/app/routes/ewt-patron/ewt-patron/ewt-patron.component.spec.ts
+++ src/app/routes/ewt-patron/ewt-patron/ewt-patron.component.spec.ts
@@ -1,25 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { EwtPatronComponent } from './ewt-patron.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { StakeState } from '../../../state/stake/stake.reducer';
+import * as stakeSelectors from '../../../state/stake/stake.selectors';
+import { LastDigitsPipe } from '../pipes/last-digits.pipe';
 
 describe('EwtPatronComponent', () => {
   let component: EwtPatronComponent;
   let fixture: ComponentFixture<EwtPatronComponent>;
+  let store: MockStore<StakeState>;
+  const setUp = () => {
+    store.overrideSelector(stakeSelectors.getBalance, '0');
+    store.overrideSelector(stakeSelectors.getAnnualReward, 10);
+    store.overrideSelector(stakeSelectors.getPerformance, 100);
+  };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EwtPatronComponent ]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [EwtPatronComponent, LastDigitsPipe],
+      providers: [
+        {provide: MatDialog, useValue: {open: () => {}}},
+        provideMockStore()
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
-    .compileComponents();
-  });
+      .compileComponents();
+    store = TestBed.inject(MockStore);
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EwtPatronComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    setUp();
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
