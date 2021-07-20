df465e2e4c11ded971bd2a9f1d900023793e4782
Vadim O <v.ostapets@spd-ukraine.com>
2021-07-20 11:30:48 +0300

    Fixed run code-coverage and show coverage page

--- karma.conf.js
+++ karma.conf.js
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
@@ -31,6 +32,11 @@ module.exports = function (config) {
         { type: 'html' },
         { type: 'text-summary' }
       ]
+    },
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, './coverage/ui'),
+      reports: ['html', 'lcovonly', 'text-summary'],
+      fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
