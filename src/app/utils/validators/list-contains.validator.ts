3a6233675cf0cc0e6b76d535ea88cde393e5c97e
amaugosomto <amaugosomto@gmail.com>
2021-07-28 08:26:26 +0100

    added more tests

--- src/app/utils/validators/list-contains.validator.ts
+++ src/app/utils/validators/list-contains.validator.ts
@@ -2,7 +2,11 @@ import { AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 export function listContainsValidator<T>(source: T[], property: string): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-      const exists = source.find(key => key[property] === control.value);
-      return exists ? {listContains: {value: control.value}} : null;
+        if (!source || !property) {
+            return null;
+        }
+
+        const exists = source.find(key => key[property] === control.value);
+        return exists ? {listContains: {value: control.value}} : null;
     };
 }
