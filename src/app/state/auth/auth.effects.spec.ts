83ed71bbf1e65fb5d29d7a78e9e13a60fc3765e7
dawidgil <dawid.gil@j-labs.pl>
2021-07-30 23:21:00 +0200

    add tests for login in auth effects.

--- src/app/state/auth/auth.effects.spec.ts
+++ src/app/state/auth/auth.effects.spec.ts
@@ -8,12 +8,13 @@ import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AuthState } from './auth.reducer';
 import { AuthEffects } from './auth.effects';
 import * as AuthActions from './auth.actions';
-import { IAM } from 'iam-client-lib';
+import { IAM, WalletProvider } from 'iam-client-lib';
 import { MatDialog } from '@angular/material/dialog';
+import { finalize } from 'rxjs/operators';
 
 describe('AuthEffects', () => {
 
-  const iamSpy = jasmine.createSpyObj('iam', ['login', 'disconnect', 'clearWaitSignatureTimer', 'waitForSignature']);
+  const iamServiceSpy = jasmine.createSpyObj('IamService', ['waitForSignature', 'clearWaitSignatureTimer', 'login', 'disconnect']);
   const dialogSpy = jasmine.createSpyObj('MatDialog', ['closeAll']);
   let actions$: ReplaySubject<any>;
   let effects: AuthEffects;
@@ -23,7 +24,7 @@ describe('AuthEffects', () => {
     TestBed.configureTestingModule({
       providers: [
         AuthEffects,
-        {provide: IamService, useValue: {iam: iamSpy}},
+        {provide: IamService, useValue: iamServiceSpy},
         {provide: MatDialog, useValue: dialogSpy},
         provideMockStore(),
         provideMockActions(() => actions$),
@@ -41,12 +42,80 @@ describe('AuthEffects', () => {
 
     it('should return action for setting metamask options ', (done) => {
       actions$.next(AuthActions.init());
-      spyOn(IAM, 'isMetamaskExtensionPresent').and.returnValue(Promise.resolve({isMetamaskPresent: true, chainId: 123}));
+      spyOn(IAM, 'isMetamaskExtensionPresent').and.returnValue(Promise.resolve({
+        isMetamaskPresent: true,
+        chainId: 123
+      }));
 
       effects.metamaskOptions$.subscribe(resultAction => {
         expect(resultAction).toEqual(AuthActions.setMetamaskLoginOptions({present: true, chainId: 123}));
         done();
       });
     });
+  });
+
+  describe('login$', () => {
+    beforeEach(() => {
+      actions$ = new ReplaySubject(1);
+    });
+
+    it('should close dialog and return login success action when login was successful', (done) => {
+      actions$.next(AuthActions.login({provider: WalletProvider.MetaMask}));
+      iamServiceSpy.login.and.returnValue(Promise.resolve(true));
+
+      effects.login$.pipe(
+        finalize(() => expect(iamServiceSpy.clearWaitSignatureTimer).toHaveBeenCalled())
+      )
+        .subscribe(resultAction => {
+          expect(iamServiceSpy.login).toHaveBeenCalledWith({
+            walletProvider: WalletProvider.MetaMask,
+            reinitializeMetamask: true
+          }, undefined);
+          expect(iamServiceSpy.waitForSignature).toHaveBeenCalled();
+          expect(resultAction).toEqual(AuthActions.loginSuccess());
+          expect(dialogSpy.closeAll).toHaveBeenCalled();
+
+          done();
+        });
+    });
+
+    it('should do not close dialog and return login failure action on login failure', (done) => {
+      actions$.next(AuthActions.login({provider: WalletProvider.MetaMask}));
+      iamServiceSpy.login.and.returnValue(Promise.resolve(false));
+
+      effects.login$.pipe(
+        finalize(() => expect(iamServiceSpy.clearWaitSignatureTimer).toHaveBeenCalled())
+      )
+        .subscribe(resultAction => {
+          expect(iamServiceSpy.waitForSignature).toHaveBeenCalled();
+          expect(resultAction).toEqual(AuthActions.loginFailure());
+
+          done();
+        });
+    });
+
+    it('should do not close dialog and return login failure action when login throws error', (done) => {
+      actions$.next(AuthActions.login({provider: WalletProvider.MetaMask}));
+      iamServiceSpy.login.and.returnValue(Promise.reject());
+
+      effects.login$
+        .subscribe(resultAction => {
+          expect(resultAction).toEqual(AuthActions.loginFailure());
+
+          done();
+        });
+    });
+
+    it('should call waitForSignature with metamask and not navigate on timeout option', (done) => {
+      actions$.next(AuthActions.login({provider: WalletProvider.MetaMask, navigateOnTimeout: false}));
+      iamServiceSpy.login.and.returnValue(Promise.resolve(true));
+
+      effects.login$
+        .subscribe(() => {
+          expect(iamServiceSpy.waitForSignature).toHaveBeenCalledWith(WalletProvider.MetaMask, true, false);
+          done();
+        });
+    });
+
   });
 });
