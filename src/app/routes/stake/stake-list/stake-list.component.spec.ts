236eeb4b3b6c2714d5924c85e5dda8feaa038230
dawidgil <dawid.gil@j-labs.pl>
2021-08-10 14:22:43 +0200

    add test for opening staking page in new window.

--- src/app/routes/stake/stake-list/stake-list.component.spec.ts
+++ src/app/routes/stake/stake-list/stake-list.component.spec.ts
@@ -1,16 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StakeListComponent } from './stake-list.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { Router } from '@angular/router';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import * as stakeSelectors from '../../../state/stake/stake.selectors';
+import { Provider } from '../../../state/stake/models/provider.interface';
+import { By } from '@angular/platform-browser';
 
 describe('StakeListComponent', () => {
   let component: StakeListComponent;
   let fixture: ComponentFixture<StakeListComponent>;
   let store: MockStore;
-  beforeEach(waitForAsync( () => {
+  let hostDebug: DebugElement;
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [StakeListComponent],
       providers: [
@@ -26,11 +29,23 @@ describe('StakeListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StakeListComponent);
     component = fixture.componentInstance;
+    hostDebug = fixture.debugElement;
   });
 
   it('should create', () => {
     fixture.detectChanges();
     store.overrideSelector(stakeSelectors.getProviders, []);
     expect(component).toBeTruthy();
+  });
+
+  it('should open new tab when clicking on provider tab', () => {
+    spyOn(window, 'open')
+    store.overrideSelector(stakeSelectors.getProviders, [{org: 'org'} as Provider]);
+
+    fixture.detectChanges();
+    const card = hostDebug.query(By.css('[data-qa-id=provider-org'));
+    card.nativeElement.click();
+
+    expect(window.open).toHaveBeenCalledWith('/staking?org=org');
   });
 });
