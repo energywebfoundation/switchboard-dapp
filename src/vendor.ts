dcbeec45865c820346e0f512f4285b9474c9de09
Vadim O <v.ostapets@spd-ukraine.com>
2021-08-17 23:41:21 +0300

    deleted sidebar

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,8 +1,6 @@
 
 import './modernizr.js'; // 'npm run modernizr' to create this file
 
-import '../node_modules/jquery-slimscroll/jquery.slimscroll.js';
-
 import '../node_modules/codemirror/lib/codemirror.js';
 import '../node_modules/codemirror/mode/javascript/javascript.js';
 import '../node_modules/codemirror/mode/xml/xml.js';
