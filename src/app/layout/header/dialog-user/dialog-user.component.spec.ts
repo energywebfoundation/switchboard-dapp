a8a4c968b0eb19e3fd34f1ebb0ed592ee9c29cc3
Vadim O <v.ostapets@spd-ukraine.com>
2021-08-18 00:09:36 +0300

    fixed run test

--- src/app/layout/header/dialog-user/dialog-user.component.spec.ts
+++ src/app/layout/header/dialog-user/dialog-user.component.spec.ts
@@ -123,5 +123,5 @@ const selectors = (hostDebug: DebugElement) => {
 
 const todayDate = () => {
   const date = new Date(Date.now());
-  return `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+  return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
 };
