71c7cb36d6d53547549479f6a5407439dd4e6792
dawidgil <83639710+dawidgil@users.noreply.github.com>
2021-09-02 12:24:28 +0200

    SWTCH-1171 remove rounding staking by taking only integer value. It should take also fractional part.
    * round staking value to last 2 digits after a dot instead of taking only integer value.
    
    * add tests for calculating amount to stake after clicking buttons.
    
    * add mocked store to tests.

--- src/app/routes/ewt-patron/stake/stake.component.spec.ts
+++ src/app/routes/ewt-patron/stake/stake.component.spec.ts
@@ -1,16 +1,40 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { StakeComponent } from './stake.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { LastDigitsPipe } from '../pipes/last-digits.pipe';
 
-xdescribe('StakeComponent', () => {
+describe('StakeComponent', () => {
   let component: StakeComponent;
   let fixture: ComponentFixture<StakeComponent>;
+  let store: MockStore;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [StakeComponent]
+      declarations: [StakeComponent, LastDigitsPipe],
+      providers: [
+        provideMockStore({
+          initialState: {
+            pool: {
+              balance: '0',
+              performance: 100,
+              annualReward: 10,
+              reward: '0',
+              organization: '',
+              userStake: null,
+              withdrawing: false,
+              organizationDetails: null,
+            }
+
+          }
+        }),
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
+
+    store = TestBed.inject(MockStore);
   }));
 
   beforeEach(() => {
@@ -21,5 +45,19 @@ xdescribe('StakeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should calculate stake amount when taking 100%', () => {
+    component.tokenAmount = 10.123;
+    component.calcStakeAmount(100);
+
+    expect(component.amountToStake.value).toEqual(10.123);
+  });
+
+  it('should calculate stake amount when taking 50%', () => {
+    component.tokenAmount = 10.12;
+    component.calcStakeAmount(50);
+
+    expect(component.amountToStake.value).toEqual(5.06);
   });
 });
