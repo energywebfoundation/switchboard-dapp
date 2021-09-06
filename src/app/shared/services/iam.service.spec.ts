8744c3b44aa559acfe6e29c90b239e92b655632e
dawidgil <dawid.gil@j-labs.pl>
2021-09-06 13:07:30 +0200

    clean up iam service spec file.

--- src/app/shared/services/iam.service.spec.ts
+++ src/app/shared/services/iam.service.spec.ts
@@ -1,19 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { IamService } from './iam.service';
-import { provideMockStore } from '@ngrx/store/testing';
-import { ToastrService } from 'ngx-toastr';
 
 describe('IamService', () => {
   let service: IamService;
-  const toastrSpy = jasmine.createSpyObj('ToastrService', ['success']);
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         IamService,
-        provideMockStore(),
-        {provide: ToastrService, useValue: toastrSpy}
       ]
     });
 
