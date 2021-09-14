7a1c075564b5c173b0271365835be6d499bd5163
dawidgil <dawid.gil@j-labs.pl>
2021-09-14 23:18:28 +0200

    adjust header component providers in test file.

--- src/app/layout/header/header.component.spec.ts
+++ src/app/layout/header/header.component.spec.ts
@@ -1,15 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { UserIdleService } from 'angular-user-idle';
 import { HeaderComponent } from './header.component';
-import { MenuService } from '../../core/menu/menu.service';
 import { IamService } from '../../shared/services/iam.service';
 import { Router } from '@angular/router';
 import { NotificationService } from '../../shared/services/notification.service';
 import { SettingsService } from '../../core/settings/settings.service';
 import { MatDialog } from '@angular/material/dialog';
-import { DomSanitizer } from '@angular/platform-browser';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { SwitchboardToastrService } from '../../shared/services/switchboard-toastr.service';
+import { provideMockStore } from '@ngrx/store/testing';
+import { LoginService } from '../../shared/services/login/login.service';
 
 xdescribe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -19,15 +18,14 @@ xdescribe('HeaderComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
       providers: [
-        {provide: UserIdleService, useValue: {}},
-        {provide: MenuService, useValue: {}},
         {provide: IamService, useValue: {}},
         {provide: Router, useValue: {}},
         {provide: SwitchboardToastrService, useValue: {}},
         {provide: NotificationService, useValue: {}},
         {provide: SettingsService, useValue: {}},
         {provide: MatDialog, useValue: {}},
-        {provide: DomSanitizer, useValue: {}},
+        {provide: LoginService, useValue: {}},
+        provideMockStore()
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
