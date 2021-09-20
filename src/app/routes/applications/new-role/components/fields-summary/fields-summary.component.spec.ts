a8b97c28cc38c9b91cb4b997b86fb23871509841
dawidgil <dawid.gil@j-labs.pl>
2021-09-20 13:57:47 +0200

    fix tests for fields summary component.

--- src/app/routes/applications/new-role/components/fields-summary/fields-summary.component.spec.ts
+++ src/app/routes/applications/new-role/components/fields-summary/fields-summary.component.spec.ts
@@ -1,17 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FieldsSummaryComponent } from './fields-summary.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('FieldsSummaryComponent', () => {
   let component: FieldsSummaryComponent;
   let fixture: ComponentFixture<FieldsSummaryComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [FieldsSummaryComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [FieldsSummaryComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FieldsSummaryComponent);
