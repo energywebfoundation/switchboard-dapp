aeb0095ace5f39c2cbe957697f2fff207f9e441b
dawidgil <dawid.gil@j-labs.pl>
2021-09-20 16:40:12 +0200

    fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -26,11 +26,11 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/testing-project'),
+      dir: require('path').join(__dirname, './coverage/'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
+        {type: 'html'},
+        {type: 'text-summary'}
       ]
     },
     coverageIstanbulReporter: {
