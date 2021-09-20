75bba89bde514dcdc307c433c210f22ae06250ba
dawidgil <dawid.gil@j-labs.pl>
2021-09-20 17:50:53 +0200

    del(deps): from minified did viewer dialog added to close dialog. Now uses directive for closing.

--- src/app/shared/directives/minified-did-viewer/minified-did-viewer-dialog.component.ts
+++ src/app/shared/directives/minified-did-viewer/minified-did-viewer-dialog.component.ts
@@ -1,18 +1,14 @@
-import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-minified-did-viewer-dialog',
   templateUrl: './minified-did-viewer-dialog.component.html',
-  styleUrls: ['./minified-did-viewer-dialog.component.scss']
+  styleUrls: ['./minified-did-viewer-dialog.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MinifiedDidViewerDialogComponent {
 
-  constructor(public dialogRef: MatDialogRef<MinifiedDidViewerDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: any) {
-  }
-
-  closeDialog() {
-    this.dialogRef.close();
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
   }
 }
