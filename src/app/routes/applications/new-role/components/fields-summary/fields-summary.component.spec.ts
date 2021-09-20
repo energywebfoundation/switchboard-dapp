2eb329e8600dbd41e64917c7e6f04eee45b6e8fa
dawidgil <dawid.gil@j-labs.pl>
2021-09-20 14:02:18 +0200

    add tests for checking getters.

--- src/app/routes/applications/new-role/components/fields-summary/fields-summary.component.spec.ts
+++ src/app/routes/applications/new-role/components/fields-summary/fields-summary.component.spec.ts
@@ -18,10 +18,48 @@ describe('FieldsSummaryComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(FieldsSummaryComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should check if isText getter is true', () => {
+    component.element = {fieldType: 'text'};
+    fixture.detectChanges();
+
+    expect(component.isText).toBeTrue();
+  });
+
+  it('should check if isNumber getter is true', () => {
+    component.element = {fieldType: 'number'};
+    fixture.detectChanges();
+
+    expect(component.isNumber).toBeTrue();
+  });
+
+  it('should check if isDate getter is true', () => {
+    component.element = {fieldType: 'date'};
+    fixture.detectChanges();
+
+    expect(component.isDate).toBeTruthy();
+  });
+
+  it('should check if isBoolean getter is true', () => {
+    component.element = {fieldType: 'boolean'};
+    fixture.detectChanges();
+
+    expect(component.isBoolean).toBeTruthy();
+  });
+
+  it('should check if getters are false', () => {
+    component.element = {fieldType: 'differentType'};
+    fixture.detectChanges();
+
+    expect(component.isText).toBeFalse();
+    expect(component.isBoolean).toBeFalse();
+    expect(component.isDate).toBeFalse();
+    expect(component.isNumber).toBeFalse();
   });
 });
