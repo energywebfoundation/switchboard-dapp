f0e49e4382ff1313394dad6612d3abddaad313ae
dawidgil <dawid.gil@j-labs.pl>
2021-09-21 13:10:22 +0200

    ref: passive asset registration.

--- src/app/routes/assets/new-passive-asset/new-passive-asset.component.ts
+++ src/app/routes/assets/new-passive-asset/new-passive-asset.component.ts
@@ -1,40 +1,24 @@
-import { Component } from '@angular/core';
-import { CancelButton } from '../../../layout/loading/loading.component';
-import { IamService } from '../../../shared/services/iam.service';
-import { LoadingService } from '../../../shared/services/loading.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { SwitchboardToastrService } from '../../../shared/services/switchboard-toastr.service';
+import { AssetService } from '../services/asset.service';
 
 @Component({
   selector: 'app-new-passive-asset',
   templateUrl: './new-passive-asset.component.html',
-  styleUrls: ['./new-passive-asset.component.scss']
+  styleUrls: ['./new-passive-asset.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NewPassiveAssetComponent {
 
-  private TOASTR_HEADER = 'Register Single Asset';
-
-  constructor(public dialogRef: MatDialogRef<NewPassiveAssetComponent>,
-              private loadingService: LoadingService,
-              private toastr: SwitchboardToastrService,
-              private iamService: IamService) {
+  constructor(private dialogRef: MatDialogRef<NewPassiveAssetComponent>,
+              private assetsService: AssetService) {
   }
 
-  async registerAsset() {
-    let success = false;
-    try {
-      this.loadingService.show('Please confirm this transaction in your connected wallet.', CancelButton.ENABLED);
-      const assetAddress = await this.iamService.iam.registerAsset();
-      this.toastr.success('New asset is successfully registered.', this.TOASTR_HEADER);
-      success = true;
-    } catch (e) {
-      console.error(e);
-      this.toastr.error(e.message || 'Could not register asset at this time. Please contact system administrator',
-        this.TOASTR_HEADER);
-    } finally {
-      this.loadingService.hide();
-      this.closeDialog(success);
-    }
+  registerAsset() {
+    this.assetsService.register().subscribe(
+      () => this.closeDialog(true),
+      () => this.closeDialog(false)
+    );
   }
 
   closeDialog(isSuccess?: boolean) {
