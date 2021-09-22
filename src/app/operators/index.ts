256ed2b699815e2043d751e2ea1c75ac709fa81a
dawidgil <dawid.gil@j-labs.pl>
2021-09-22 10:35:58 +0200

    move filter-providers operator to stake slice folder.

--- src/app/operators/index.ts
+++ src/app/operators/index.ts
@@ -1,4 +1,3 @@
-export * from './filter-providers/filter-providers';
 export * from './retry-when-with-delay/retry-when-with-delay';
 export * from './truthy/truthy';
 export * from './map-claims-profile/map-claims-profile';
