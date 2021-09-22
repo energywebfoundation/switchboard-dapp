3047c2dcfafe154219e37d2e57801f9e6f887f28
dawidgil <83639710+dawidgil@users.noreply.github.com>
2021-09-22 11:56:52 +0200

    ref: did format minifier. add tests to it.

--- src/app/shared/pipes/did-format-minifier.pipe.ts
+++ src/app/shared/pipes/did-format-minifier.pipe.ts
@@ -1,19 +1,20 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+const CHARACTERS_AFTER_LAST_COLON = 7;
+const LAST_CHARACTERS = 5;
+
 @Pipe({
   name: 'didFormatMinifier'
 })
 export class DidFormatMinifierPipe implements PipeTransform {
 
-  transform(value: string, ...args: unknown[]): unknown {
-    let retVal = value;
-
-    if (value && value.length > 10) {
-      const lastColonIndex = value.lastIndexOf(':');
-      retVal = `${value.substr(0, lastColonIndex + 7)}...${value.substr(value.length - 5)}`;
+  transform(value: string): string | undefined {
+    if (!value) {
+      return value;
     }
 
-    return retVal;
+    const lastColonIndex = value.lastIndexOf(':');
+    return `${value.substr(0, lastColonIndex + CHARACTERS_AFTER_LAST_COLON)}...${value.substr(value.length - LAST_CHARACTERS)}`;
   }
 
 }
