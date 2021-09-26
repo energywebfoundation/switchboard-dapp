31fb1bd0b85ed512cbecdb8232ed0a7832cca849
dawidgil <dawid.gil@j-labs.pl>
2021-09-26 23:28:21 +0200

    fix testing file for actions menu

--- src/app/routes/applications/actions/actions-menu/actions-menu.component.spec.ts
+++ src/app/routes/applications/actions/actions-menu/actions-menu.component.spec.ts
@@ -1,17 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ActionsMenuComponent } from './actions-menu.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('ActionsMenuComponent', () => {
   let component: ActionsMenuComponent;
   let fixture: ComponentFixture<ActionsMenuComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ActionsMenuComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ActionsMenuComponent],
+      imports: [MatIconTestingModule, MatMenuModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ActionsMenuComponent);
