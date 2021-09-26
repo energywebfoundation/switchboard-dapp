313a11c336a1178425856dffdbcefd9fc1435b47
dawidgil <dawid.gil@j-labs.pl>
2021-09-26 23:28:32 +0200

    fix testing file for application list component.

--- src/app/routes/applications/application-list/application-list.component.spec.ts
+++ src/app/routes/applications/application-list/application-list.component.spec.ts
@@ -1,21 +1,44 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ApplicationListComponent } from './application-list.component';
+import { MatDialog } from '@angular/material/dialog';
+import { dialogSpy, iamServiceSpy, loadingServiceSpy, toastrSpy } from '@tests';
+import { LoadingService } from '../../../shared/services/loading.service';
+import { SwitchboardToastrService } from '../../../shared/services/switchboard-toastr.service';
+import { IamService } from '../../../shared/services/iam.service';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { ApplicationSelectors } from '@state';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('ApplicationListComponent', () => {
   let component: ApplicationListComponent;
   let fixture: ComponentFixture<ApplicationListComponent>;
+  let store: MockStore;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ApplicationListComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ApplicationListComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        {provide: MatDialog, useValue: dialogSpy},
+        {provide: LoadingService, useValue: loadingServiceSpy},
+        {provide: SwitchboardToastrService, useValue: toastrSpy},
+        {provide: IamService, useValue: iamServiceSpy},
+        provideMockStore()
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
-  });
+
+    store = TestBed.inject(MockStore);
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ApplicationListComponent);
     component = fixture.componentInstance;
+    store.overrideSelector(ApplicationSelectors.getFilteredList, []);
+    store.overrideSelector(ApplicationSelectors.getFilters, {organization: '', application: '', role: ''});
     fixture.detectChanges();
   });
 
