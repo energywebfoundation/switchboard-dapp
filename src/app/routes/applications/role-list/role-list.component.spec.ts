b66a6ffa7dbe077629b1eac6bf2a879ab2fe17e8
dawidgil <dawid.gil@j-labs.pl>
2021-09-28 10:40:15 +0200

    add more tests to role list.

--- src/app/routes/applications/role-list/role-list.component.spec.ts
+++ src/app/routes/applications/role-list/role-list.component.spec.ts
@@ -5,7 +5,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { dialogSpy } from '@tests';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
-import { RoleSelectors } from '@state';
+import { RoleActions, RoleSelectors } from '@state';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { RoleType } from '../new-role/new-role.component';
 
@@ -13,6 +13,13 @@ describe('RoleListComponent', () => {
   let component: RoleListComponent;
   let fixture: ComponentFixture<RoleListComponent>;
   let store: MockStore;
+
+  const setup = (list?) => {
+    store.overrideSelector(RoleSelectors.getFilteredList, list || []);
+    store.overrideSelector(RoleSelectors.getFilters, {organization: '', application: '', role: ''});
+    store.overrideSelector(RoleSelectors.isFilterVisible, false);
+    fixture.detectChanges();
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -32,29 +39,45 @@ describe('RoleListComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RoleListComponent);
     component = fixture.componentInstance;
-    store.overrideSelector(RoleSelectors.getFilteredList, []);
-    store.overrideSelector(RoleSelectors.getFilters, {organization: '', application: '', role: ''});
-    store.overrideSelector(RoleSelectors.isFilterVisible, false);
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    setup();
     expect(component).toBeTruthy();
   });
 
   it('should return truthy for checking isOrgType and falsy for others', () => {
+    setup();
     expect(component.isOrgType(RoleType.ORG)).toBeTrue();
     expect(component.isAppType(RoleType.ORG)).toBeFalse();
     expect(component.isCustomType(RoleType.ORG)).toBeFalse();
   });
+
   it('should return truthy for checking isAppType and falsy for others', () => {
+    setup();
     expect(component.isOrgType(RoleType.APP)).toBeFalse();
     expect(component.isAppType(RoleType.APP)).toBeTrue();
     expect(component.isCustomType(RoleType.APP)).toBeFalse();
   });
+
   it('should return truthy for checking isCustomType and falsy for others', () => {
+    setup();
     expect(component.isOrgType(RoleType.CUSTOM)).toBeFalse();
     expect(component.isAppType(RoleType.CUSTOM)).toBeFalse();
     expect(component.isCustomType(RoleType.CUSTOM)).toBeTrue();
+  });
+
+  it('should refresh list after successful edition', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    setup();
+    component.edit();
+
+    expect(dispatchSpy).toHaveBeenCalledWith(RoleActions.getList());
+  });
+
+  it('should set list to table', () => {
+    setup([{}, {}, {}]);
+
+    expect(component.dataSource.data.length).toBe(3);
   });
 });
