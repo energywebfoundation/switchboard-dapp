438ae4eb4a675bddcee330ad1a9e2d677adebec2
dawidgil <dawid.gil@j-labs.pl>
2021-09-28 15:08:28 +0200

    fix linter.

--- src/app/routes/applications/actions/actions-menu/actions-menu.component.ts
+++ src/app/routes/applications/actions/actions-menu/actions-menu.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-actions-menu',
   templateUrl: './actions-menu.component.html',
   styleUrls: ['./actions-menu.component.scss']
 })
-export class ActionsMenuComponent implements OnInit {
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
+export class ActionsMenuComponent {
 }
