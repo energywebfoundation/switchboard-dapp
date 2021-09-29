8dfb4f7f4b403466562e22e605b5ac36d5935de8
dawidgil <dawid.gil@j-labs.pl>
2021-09-29 13:51:49 +0200

    combine needed data to display information about account.

--- src/app/routes/ewt-patron/staking-header/staking-header.component.ts
+++ src/app/routes/ewt-patron/staking-header/staking-header.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import * as authSelectors from '../../../state/auth/auth.selectors';
 import { Store } from '@ngrx/store';
 import * as AuthActions from '../../../state/auth/auth.actions';
+import { combineLatest } from 'rxjs';
+import { AuthSelectors, UserClaimSelectors } from '@state';
 
 @Component({
   selector: 'app-staking-header',
@@ -11,8 +13,14 @@ import * as AuthActions from '../../../state/auth/auth.actions';
 })
 export class StakingHeaderComponent {
   loggedIn$ = this.store.select(authSelectors.isUserLoggedIn);
+  accountInfo$ = combineLatest([
+    this.store.select(AuthSelectors.getWalletProvider),
+    this.store.select(AuthSelectors.getAccountInfo),
+    this.store.select(UserClaimSelectors.getUserName),
+  ]);
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {
+  }
 
   logOut() {
     this.store.dispatch(AuthActions.logout());
