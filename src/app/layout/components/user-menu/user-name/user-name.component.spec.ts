6d323878ef7e3de139fe6a0bc052d559a76834f5
dawidgil <dawid.gil@j-labs.pl>
2021-09-30 13:29:04 +0200

    add test that checks if username is displayed.

--- src/app/layout/components/user-menu/user-name/user-name.component.spec.ts
+++ src/app/layout/components/user-menu/user-name/user-name.component.spec.ts
@@ -1,25 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { UserNameComponent } from './user-name.component';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 describe('UserNameComponent', () => {
   let component: UserNameComponent;
   let fixture: ComponentFixture<UserNameComponent>;
+  let hostDebug: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [UserNameComponent]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(UserNameComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostDebug = fixture.debugElement;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should display user name value', () => {
+    component.userName = 'user';
+    fixture.detectChanges();
+    const user = getElement(hostDebug)('menu-user-name').nativeElement;
+    expect(user.innerText).toContain('user');
+  });
 });
+const getElement = (hostDebug) => (id, postSelector = '') => hostDebug.query(By.css(`[data-qa-id=${id}] ${postSelector}`));
