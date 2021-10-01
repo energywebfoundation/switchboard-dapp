b1fb8d2369f90a48ccce742cc2050211ef64a2b8
dawidgil <dawid.gil@j-labs.pl>
2021-10-01 14:19:08 +0200

    update excluded aliases to webpack config.

--- extra-webpack.config.js
+++ extra-webpack.config.js
@@ -3,7 +3,28 @@ const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = {
     plugins: [
         new NodePolyfillPlugin({
-            excludeAliases: ['_stream_readable']
+            excludeAliases: [
+                "Buffer",
+                "console",
+                "process",
+                "assert",
+                "buffer",
+                "console",
+                "constants",
+                "domain",
+                "events",
+                "path",
+                "punycode",
+                "process",
+                "querystring",
+                "_stream_duplex",
+                "_stream_passthrough",
+                "_stream_readable",
+                "_stream_transform",
+                "_stream_writable",
+                "string_decoder",
+                "sys", "timers", "tty", "url", "util", "vm", "zlib"
+            ]
         })
     ]
 }
