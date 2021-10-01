2d3cd29cf855124d74ee6f855d771b410e4ba374
dawidgil <dawid.gil@j-labs.pl>
2021-10-01 12:02:03 +0200

    chore(deps): update angular to version 12

--- patch.js
+++ patch.js
@@ -1,18 +1,20 @@
+/**
+ *  This file is for patching import for nats.ws
+ *  With Angular 12 current import inside iam-client-lib is making an error while building application.
+ *  This patch fixes import before building application.
+ */
+
 // eslint-disable-next-line
 const fs = require('fs');
-const f =
-  'node_modules/@angular-devkit/build-angular/src/webpack/configs/browser.js';
+const file = 'node_modules/iam-client-lib/dist/iam-client-lib.esm.js';
 
-fs.readFile(f, 'utf8', function (err, data) {
+fs.readFile(file, 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  var result = data.replace(
-    /node: false/g,
-    'node: { crypto: true, stream: true, buffer: false }'
-  );
+  const result = data.replace('nats.ws/lib/src/mod.js', 'nats.ws');
 
-  fs.writeFile(f, result, 'utf8', function (err) {
+  fs.writeFile(file, result, 'utf8', function (err) {
     if (err) return console.log(err);
   });
 });
