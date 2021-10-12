a79994fed4442ce4a4e97591acef7bdd25aff199
dawidgil <dawid.gil@j-labs.pl>
2021-10-12 21:15:24 +0200

    add test to did-qr-code component.

--- src/app/routes/assets/did-qr-code/did-qr-code.component.spec.ts
+++ src/app/routes/assets/did-qr-code/did-qr-code.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
-import { DidQrCodeComponent } from './qr-code.component';
+import { DidQrCodeComponent } from './did-qr-code.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { dialogSpy } from '@tests';
 
 describe('QrCodeComponent', () => {
   let component: DidQrCodeComponent;
   let fixture: ComponentFixture<DidQrCodeComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [DidQrCodeComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {provide: MatDialogRef, useValue: dialogSpy},
+        {provide: MAT_DIALOG_DATA, useValue: {id: 'did:ethr:test'}}
+      ],
+      declarations: [DidQrCodeComponent],
+      schemas: [NO_ERRORS_SCHEMA]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DidQrCodeComponent);
@@ -21,5 +29,9 @@ describe('QrCodeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should did from id', () => {
+    expect(component.did).toBe('did:ethr:test');
   });
 });
--- src/app/routes/assets/did-qr-code/did-qr-code.component.ts
+++ src/app/routes/assets/did-qr-code/did-qr-code.component.ts
@@ -1,21 +1,20 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { Asset } from 'iam-client-lib';
 
 @Component({
   selector: 'app-did-qr-code',
   templateUrl: './did-qr-code.component.html',
-  styleUrls: ['./did-qr-code.component.scss']
+  styleUrls: ['./did-qr-code.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DidQrCodeComponent implements OnInit {
-  did: string;
-
-  constructor(public dialogRef: MatDialogRef<DidQrCodeComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Asset) {
+export class DidQrCodeComponent {
+  get did() {
+    return this.data.id;
   }
 
-  ngOnInit(): void {
-    this.did = this.data.id;
+  constructor(public dialogRef: MatDialogRef<DidQrCodeComponent>,
+              @Inject(MAT_DIALOG_DATA) private data: Asset) {
   }
 
 }
