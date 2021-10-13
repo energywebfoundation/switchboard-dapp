bd987406c71c285c641ee0aab13a20060df3c012
dawidgil <dawid.gil@j-labs.pl>
2021-10-13 23:44:22 +0200

    fix: comparing dates.

--- src/app/utils/functions/deep-equal-objects/deep-equal-objects.ts
+++ src/app/utils/functions/deep-equal-objects/deep-equal-objects.ts
@@ -12,15 +12,22 @@ export const deepEqualObjects = (defaultObject, objectToCompare): boolean => {
   return defaultKeys.every(key => {
     const val1 = defaultObject[key];
     const val2 = objectToCompare[key];
-    const areObjects = isObject(val1) && isObject(val2);
 
-    const equalObjects = areObjects && deepEqualObjects(val1, val2);
+    const areObjects = isObject(val1) && isObject(val2);
+    const areDates = isDate(val1) && isDate(val2);
+
+    const equalDates = areDates && val1?.toString() === val2?.toString();
+    const equalObjects = areObjects && !areDates && deepEqualObjects(val1, val2);
     const equalValues = !areObjects && val1 === val2;
 
-    return (equalObjects || equalValues);
+    return (equalObjects || equalValues || equalDates);
   });
 };
 
 const isObject = (object) => {
   return object != null && typeof object === 'object';
+};
+
+const isDate = (object) => {
+  return object instanceof Date; //&& Object.toString.call(object) === '[object Date]'
 };
