ff05896785201be66bfa7fb23941fb1c87ca146a
dawidgil <dawid.gil@j-labs.pl>
2021-10-25 14:34:03 +0200

    del: comments from env service factory.

--- src/app/shared/services/env/env.service.factory.ts
+++ src/app/shared/services/env/env.service.factory.ts
@@ -1,16 +1,15 @@
 import { EnvService } from './env.service';
 
 export const EnvServiceFactory = () => {
-  // Create env
   const env = new EnvService();
 
-  // Read environment variables from browser window
-  const browserWindow = window || {};
-  const browserWindowEnv = browserWindow['__env'] || {};
+  const getBrowserEnv = () => {
+    const browserWindow = window || {};
+    return browserWindow['__env'] || {};
+  };
 
-  // Assign environment variables from browser window to env
-  // In the current implementation, properties from env.js overwrite defaults from the EnvService.
-  // If needed, a deep merge can be performed here to merge properties instead of overwriting them.
+  const browserWindowEnv = getBrowserEnv();
+
   for (const key in browserWindowEnv) {
     if (browserWindowEnv.hasOwnProperty(key)) {
       env[key] = window['__env'][key];
