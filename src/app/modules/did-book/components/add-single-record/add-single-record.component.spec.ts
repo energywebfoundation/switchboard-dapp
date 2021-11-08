d3a16b2aa488980cce31bc2f9e5b24ad075b873f
dawidgil <dawid.gil@j-labs.pl>
2021-11-08 22:35:30 +0100

    add: tests for single record component.

--- src/app/modules/did-book/components/add-single-record/add-single-record.component.spec.ts
+++ src/app/modules/did-book/components/add-single-record/add-single-record.component.spec.ts
@@ -1,17 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddSingleRecordComponent } from './add-single-record.component';
+import { DidBookService } from '../../services/did-book.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { dialogSpy } from '@tests';
 
 describe('AddSingleRecordComponent', () => {
   let component: AddSingleRecordComponent;
   let fixture: ComponentFixture<AddSingleRecordComponent>;
+  let didBookServiceSpy = jasmine.createSpyObj(DidBookService, ['add']);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [AddSingleRecordComponent]
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [AddSingleRecordComponent],
+      providers: [
+        {provider: DidBookService, useValue: didBookServiceSpy},
+        {provider: MatDialogRef, useValue: dialogSpy},
+        {provider: MAT_DIALOG_DATA, useValue: {did: 'did'}}
+      ]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AddSingleRecordComponent);
@@ -21,5 +30,16 @@ describe('AddSingleRecordComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add record and close dialog', () => {
+    component.addHandler({did: 'did', label: 'label'});
+    expect(didBookServiceSpy.add).toHaveBeenCalled();
+    expect(dialogSpy.close).toHaveBeenCalled();
+  });
+
+  it('should close dialog when handling cancel', () => {
+    component.cancelHandler();
+    expect(dialogSpy.close).toHaveBeenCalled();
   });
 });
