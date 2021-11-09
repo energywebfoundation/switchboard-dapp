1f272d0a169b1b82778605c825444d43f608a5ad
dawidgil <dawid.gil@j-labs.pl>
2021-11-09 13:51:11 +0100

    fix: running tests for select did component.

--- src/app/modules/did-book/components/select-did/select-did.component.spec.ts
+++ src/app/modules/did-book/components/select-did/select-did.component.spec.ts
@@ -5,6 +5,14 @@ import { DidBookService } from '../../services/did-book.service';
 import { MatDialog } from '@angular/material/dialog';
 import { dialogSpy } from '@tests';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { of } from 'rxjs';
+import { MatInputModule } from '@angular/material/input';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('SelectDidComponent', () => {
   let component: SelectDidComponent;
@@ -13,6 +21,15 @@ describe('SelectDidComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [SelectDidComponent],
+      imports: [
+        ReactiveFormsModule,
+        MatAutocompleteModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatSelectModule,
+        MatInputModule,
+        NoopAnimationsModule
+      ],
       providers: [
         {provide: DidBookService, useValue: didBookServiceSpy},
         {provide: MatDialog, useValue: dialogSpy}
@@ -25,10 +42,11 @@ describe('SelectDidComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SelectDidComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    didBookServiceSpy.getList$.and.returnValue(of([]));
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
 });
