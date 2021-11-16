6eff6e55fb4ad07d1bfa1ca0f6e783c5958cdf28
dawidgil <dawid.gil@j-labs.pl>
2021-11-16 14:18:19 +0100

    chg: get always last part of did after splitting.

--- src/app/utils/functions/extract-address/extract-address.ts
+++ src/app/utils/functions/extract-address/extract-address.ts
@@ -2,7 +2,7 @@ import { didRegex } from '../../validators/is-hex/is-hex.validator';
 
 export const extractAddress = (did: string): string => {
   if (didRegex.test(did.trim())) {
-    return did.split(':')[2];
+    return did.split(':').pop();
   }
   return did;
 };
