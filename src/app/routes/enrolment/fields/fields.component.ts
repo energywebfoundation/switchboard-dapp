51a8e0e723e56dd96ac7ee95729d5fc9ac7b2a6a
dawidgil <dawid.gil@j-labs.pl>
2021-11-18 10:45:27 +0100

    ref: cleanup fields component. Remove lint error.

--- src/app/routes/enrolment/fields/fields.component.ts
+++ src/app/routes/enrolment/fields/fields.component.ts
@@ -1,19 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { KeyValue } from '@angular/common';
 
 @Component({
   selector: 'app-fields',
   templateUrl: './fields.component.html',
-  styleUrls: ['./fields.component.scss']
+  styleUrls: ['./fields.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FieldsComponent implements OnInit {
+export class FieldsComponent {
   @Input() title: string;
   @Input() fieldsList: KeyValue<string, string | number>[];
-
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
 }
