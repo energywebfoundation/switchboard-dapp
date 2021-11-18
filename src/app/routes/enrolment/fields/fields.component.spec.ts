63008e4c49303d6eda39bf81580a3976f47acd27
dawidgil <dawid.gil@j-labs.pl>
2021-11-18 10:54:42 +0100

    add: tests for checking if data is displayed in fields component.

--- src/app/routes/enrolment/fields/fields.component.spec.ts
+++ src/app/routes/enrolment/fields/fields.component.spec.ts
@@ -1,25 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FieldsComponent } from './fields.component';
+import { DebugElement } from '@angular/core';
+import { getElement } from '@tests';
 
 describe('FieldsComponent', () => {
   let component: FieldsComponent;
   let fixture: ComponentFixture<FieldsComponent>;
+  let hostDebug: DebugElement;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       declarations: [FieldsComponent]
     })
       .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FieldsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostDebug = fixture.debugElement;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
+
+  it('should check if title is displayed', () => {
+    component.title = 'Example title';
+    component.fieldsList = [{key: 'key', value: 'value'}];
+    fixture.detectChanges();
+    const {title} = getSelectors(hostDebug);
+
+    expect(title.innerText).toContain(component.title);
+  });
+
+  it('should check if key and value are displayed', () => {
+    component.fieldsList = [{key: 'key', value: 'value'}];
+    fixture.detectChanges();
+
+    const {firstKey, firstValue} = getSelectors(hostDebug);
+
+    expect(firstKey.innerText).toContain(component.fieldsList[0].key);
+    expect(firstValue.innerText).toContain(component.fieldsList[0].value);
+  });
 });
+const getSelectors = (hostDebug) => {
+  return {
+    title: getElement(hostDebug)('field-list-title')?.nativeElement,
+    firstKey: getElement(hostDebug)('field-list-key-0')?.nativeElement,
+    firstValue: getElement(hostDebug)('field-list-value-0')?.nativeElement,
+  };
+
+};
