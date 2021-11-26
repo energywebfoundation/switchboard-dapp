c9e6bfe9a5878873dafd3d8a5002d09890edaa13
dawidgil <dawid.gil@j-labs.pl>
2021-11-26 14:50:55 +0100

    add: tests for confirmation dialog component.

--- src/app/routes/widgets/confirmation-dialog/confirmation-dialog.component.spec.ts
+++ src/app/routes/widgets/confirmation-dialog/confirmation-dialog.component.spec.ts
@@ -1,16 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ConfirmationDialogComponent } from './confirmation-dialog.component';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ConfirmationDialogComponent, ConfirmationDialogData } from './confirmation-dialog.component';
+import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconTestingModule } from '@angular/material/icon/testing';
+import { dialogSpy, getElement } from '@tests';
+
+class ConfirmDialogDataMock {
+  setData(value: ConfirmationDialogData) {
+    Object.assign(this, value);
+  }
+}
 
 describe('ConfirmationDialogComponent', () => {
   let component: ConfirmationDialogComponent;
   let fixture: ComponentFixture<ConfirmationDialogComponent>;
-
+  let mockDialogData: ConfirmDialogDataMock;
+  let hostDebug: DebugElement;
   beforeEach(() => {
+    mockDialogData = new ConfirmDialogDataMock();
     TestBed.configureTestingModule({
       declarations: [ConfirmationDialogComponent],
       imports: [
@@ -18,8 +27,8 @@ describe('ConfirmationDialogComponent', () => {
         MatIconTestingModule,
       ],
       providers: [
-        {provide: MAT_DIALOG_DATA, useValue: {}},
-        {provide: MatDialogRef, useValue: {}}
+        {provide: MAT_DIALOG_DATA, useValue: mockDialogData},
+        {provide: MatDialogRef, useValue: dialogSpy}
       ],
       schemas: [NO_ERRORS_SCHEMA]
     })
@@ -29,10 +38,77 @@ describe('ConfirmationDialogComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfirmationDialogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    hostDebug = fixture.debugElement;
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should not display icon', () => {
+    fixture.detectChanges();
+    const {icon} = getSelectors(hostDebug);
+
+    expect(icon).toBeUndefined();
+  });
+
+  it('should display icon', () => {
+    mockDialogData.setData({svgIcon: 'svg-icon'});
+    fixture.detectChanges();
+    const {icon} = getSelectors(hostDebug);
+    expect(component.svgIcon).toEqual('svg-icon');
+
+    expect(icon).toBeTruthy();
+  });
+
+  it('should display default text for header when it is not specified', () => {
+    fixture.detectChanges();
+    const {header} = getSelectors(hostDebug);
+
+    expect(header.innerText).toContain('Confirm');
+  });
+
+  it('should display text from data', () => {
+    mockDialogData.setData({header: 'header text'});
+    fixture.detectChanges();
+    const {header} = getSelectors(hostDebug);
+
+    expect(header.innerText).toContain('header text');
+  });
+
+  it('should display discard buttons', () => {
+    mockDialogData.setData({isDiscardButton: true});
+    fixture.detectChanges();
+    const {continueBtn, discardBtn} = getSelectors(hostDebug);
+
+    expect(continueBtn).toBeTruthy();
+    expect(discardBtn).toBeTruthy();
+  });
+
+  it('should display proceed buttons', () => {
+    mockDialogData.setData({isProceedButton: true});
+    fixture.detectChanges();
+    const {proceedBtn, cancelBtn} = getSelectors(hostDebug);
+
+    expect(proceedBtn).toBeTruthy();
+    expect(cancelBtn).toBeTruthy();
+  });
+
+  it('should check if calling cancel will propagate value', () => {
+    component.close(true);
+    expect(dialogSpy.close).toHaveBeenCalledWith(true);
+  });
 });
+const getSelectors = (hostDebug) => {
+  return {
+    icon: getElement(hostDebug)('icon')?.nativeElement,
+    header: getElement(hostDebug)('header')?.nativeElement,
+    message: getElement(hostDebug)('message')?.nativeElement,
+    cancelBtn: getElement(hostDebug)('cancel')?.nativeElement,
+    confirmBtn: getElement(hostDebug)('confirm')?.nativeElement,
+    continueBtn: getElement(hostDebug)('continue')?.nativeElement,
+    discardBtn: getElement(hostDebug)('discard')?.nativeElement,
+    proceedBtn: getElement(hostDebug)('proceed')?.nativeElement
+  };
+};
--- src/app/routes/widgets/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/routes/widgets/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 export interface ConfirmationDialogData {
-  message: string;
+  message?: string;
   header?: string;
   isDiscardButton?: boolean;
   isProceedButton?: boolean;
@@ -15,19 +15,22 @@ export interface ConfirmationDialogData {
   styleUrls: ['./confirmation-dialog.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ConfirmationDialogComponent {
+export class ConfirmationDialogComponent implements OnInit {
   isDiscardButton = false;
   isProceedButton = false;
   svgIcon;
 
   constructor(private dialogRef: MatDialogRef<ConfirmationDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: ConfirmationDialogData) {
-    this.svgIcon = data?.svgIcon;
-    this.isDiscardButton = data?.isDiscardButton;
-    this.isProceedButton = data?.isProceedButton;
   }
 
-  closeMe(accept: boolean) {
+  ngOnInit() {
+    this.svgIcon = this.data?.svgIcon;
+    this.isDiscardButton = this.data?.isDiscardButton;
+    this.isProceedButton = this.data?.isProceedButton;
+  }
+
+  close(accept: boolean) {
     this.dialogRef.close(accept);
   }
 }
