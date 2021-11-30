b71eb52071cbcd9a03737ce8abb990411feca908
dawidgil <dawid.gil@j-labs.pl>
2021-11-30 09:26:04 +0100

    add: redirect from asset page when disabling experimental features.

--- src/app/state/settings/settings.effects.ts
+++ src/app/state/settings/settings.effects.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import * as  SettingsActions from './settings.actions';
+import * as SettingsActions from './settings.actions';
 import { SettingsStorage } from './models/settings-storage';
+import { UrlService } from '../../shared/services/url-service/url.service';
+import { filter, map, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class SettingsEffects {
 
+  redirectFromAssetsToDashboard = createEffect(() =>
+    this.actions$
+      .pipe(
+        ofType(SettingsActions.disableExperimental),
+        switchMap(() => this.urlService.current
+          .pipe(
+            filter((url) => url.includes('assets')),
+            map(() => this.urlService.goTo('dashboard'))
+          )
+        )
+      ), {dispatch: false}
+  );
+
   constructor(private actions$: Actions,
-              private store: Store) {
+              private store: Store,
+              private urlService: UrlService) {
     this.enableExperimental();
   }
 
