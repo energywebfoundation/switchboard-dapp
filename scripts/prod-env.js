7694d59f474c03017daa3e912af0bee1948238cb
Passerino <bartosz.d.wrobel@gmail.com>
2021-12-08 16:10:25 +0100

    feat(nats): nats env name
    NATS_ENVIRONMENT_NAME is required for correct nats events namespace subscribtion

--- scripts/prod-env.js
+++ scripts/prod-env.js
@@ -9,6 +9,7 @@ fs.readFile(envConfig, 'utf8', function (err, data) {
         .replace('cacheServerUrl = \'https://identitycache-dev.energyweb.org/v1\'', 'cacheServerUrl = \'https://volta-identitycache.energyweb.org/v1\'')
         .replace('natsServerUrl = \'https://identityevents-dev.energyweb.org/\'', 'natsServerUrl = \'https://volta-identityevents.energyweb.org/\'')
         .replace('kmsServerUrl = undefined', 'kmsServerUrl = \'https://kms.energyweb.org/connect/new\'')
+        .replace('natsEnvironmentName = \'ewf-dev\'', 'natsEnvironmentName = \'ewf-prod\'')
 
     fs.writeFile(envConfig, result, 'utf8', function (err) {
         if (err) return console.log(err);
