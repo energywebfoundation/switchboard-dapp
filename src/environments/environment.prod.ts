a95adf6e390e191e2b6a5a95533c3b96cd17597a
dawidgil <83639710+dawidgil@users.noreply.github.com>
2021-12-20 11:43:48 +0100

    feat: use angular environments to build applications.
    * chg: use angular environments to build applications.
    
    * del: staking pool factory address from iam.service initialization.
    
    * del: generic feature toggle as it is not used anymore.
    
    * del: remove not needed deploys from deploy.yml file as for deploying there is cf-deploy.yml. Leave only stedin deploy.
    
    * fix: build for development branch.
    
    * del: IDENTITY_CONTRACT_ADDRESS
    
    * del: staking pool factory address property from environment.
    
    * del: env.js file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,15 +2,17 @@ import { constants } from './constants';
 
 export const environment = {
   production: true,
-  cacheServerUrl: 'https://volta-identitycache.energyweb.org/v1',
-  natsServerUrl: 'https://volta-identityevents.energyweb.org/',
+  theme: 'default',
+  application: true,
+
+  rpcUrl: 'https://rpc.energyweb.org/',
+  chainId: 246,
+  cacheServerUrl: 'https://identitycache.energyweb.org/v1',
+  natsServerUrl: 'https://identityevents.energyweb.org/',
   kmsServerUrl: 'https://kms.energyweb.org/connect/new',
-  walletConnectOptions: {
-    // rpcUrl: 'https://volta-rpc.energyweb.org/',
-    // rpcUrl: 'https://volta-internal-archive.energyweb.org/',
-    rpcUrl: 'https://volta-rpc-vkn5r5zx4ke71f9hcu0c.energyweb.org/',
-    chainId: 73799
-  },
-  featureVisible: false,
+  claimManagerAddress: '0x23b026631A6f265d17CFee8aa6ced1B244f3920C',
+  showAzureLoginOption: false,
+  natsEnvironmentName: 'ewf-prod',
+  rootNamespace: 'auth.ewc',
   ...constants
 };
--- src/environments/environment.stag.ts
+++
@@ -1,6 +0,0 @@
-import { constants } from './constants';
-
-export const environment = {
-  production: true,
-  ...constants
-};
