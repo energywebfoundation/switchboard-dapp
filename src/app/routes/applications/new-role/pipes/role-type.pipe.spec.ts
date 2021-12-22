2ca7eb85b3c5c828f8ce8b7f4f62faa9c756935b
dawidgil <dawid.gil@j-labs.pl>
2021-12-22 12:03:46 +0100

    chore: add tests for role type pipe.

--- src/app/routes/applications/new-role/pipes/role-type.pipe.spec.ts
+++ src/app/routes/applications/new-role/pipes/role-type.pipe.spec.ts
@@ -1,8 +1,22 @@
 import { RoleTypePipe } from './role-type.pipe';
+import { RoleTypeEnum } from '../new-role.component';
 
 describe('RoleTypePipe', () => {
+  let pipe: RoleTypePipe;
+
+  beforeEach(() => {
+    pipe = new RoleTypePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new RoleTypePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return application when type is app', () => {
+    expect(pipe.transform(RoleTypeEnum.APP)).toEqual('Application');
+  });
+
+  it('should return organization when type is org', () => {
+    expect(pipe.transform(RoleTypeEnum.ORG)).toEqual('Organization');
   });
 });
