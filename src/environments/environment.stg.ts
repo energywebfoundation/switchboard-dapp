715e7db711545287ae32d18af409c3550a123dbf
dawidgil <83639710+dawidgil@users.noreply.github.com>
2022-01-17 11:28:29 +0100

    fix(build): staging environment urls

--- src/environments/environment.stg.ts
+++ src/environments/environment.stg.ts
@@ -8,7 +8,7 @@ export const environment = {
   rpcUrl: 'https://volta-rpc-vkn5r5zx4ke71f9hcu0c.energyweb.org/',
   chainId: 73799,
   cacheServerUrl: 'https://identitycache-staging.energyweb.org/v1',
-  natsServerUrl: 'https://identityevents-staging.energyweb.org//',
+  natsServerUrl: 'https://identityevents-staging.energyweb.org/',
   kmsServerUrl: 'https://kms.energyweb.org/connect/new',
   claimManagerAddress: '0xC3dD7ED75779b33F5Cfb709E0aB02b71fbFA3210',
   showAzureLoginOption: true,
@@ -16,7 +16,7 @@ export const environment = {
   rootNamespace: 'iam.ewc',
 
   fullNetworkName: 'EnergyWeb Volta Chain',
-  networkName: 'EnergyWeb Volta Chain',
+  networkName: 'Volta',
   currencyName: 'Volta Token',
   currencySymbol: 'VT',
   blockExplorerUrl: 'https://volta-explorer.energyweb.org',
