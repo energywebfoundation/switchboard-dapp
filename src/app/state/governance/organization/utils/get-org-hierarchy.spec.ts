2082e48ca4f8dca8ee320b70969adb360fc29cc6
John Henderson <john.henderson@energyweb.org>
2022-01-20 10:56:20 +0100

    example typemoq test

--- src/app/state/governance/organization/utils/get-org-hierarchy.spec.ts
+++ src/app/state/governance/organization/utils/get-org-hierarchy.spec.ts
@@ -1,4 +1,6 @@
+import * as TypeMoq from "typemoq";
 import { getOrgHierarchy } from './get-org-hierarchy';
+import { OrganizationProvider } from '../models/organization-provider.interface';
 
 describe('getOrgHierarchy', () => {
   it('should return the same list when organization is undefined', () => {
@@ -34,19 +36,14 @@ describe('getOrgHierarchy', () => {
   });
 
   it('should remove last element from hierarchy', () => {
-    const result = getOrgHierarchy([
-        {
-          'name': 'secondsub',
-          'namespace': 'secondsub.realsub.testorg.iam.ewc',
-        },
-        {
-          'name': 'subsub',
-          'namespace': 'subsub.testorg.iam.ewc',
-        }] as any,
-      {
-        'name': 'secondsub',
-        'namespace': 'secondsub.realsub.testorg.iam.ewc',
-      } as any);
+    const secondSubMock = TypeMoq.Mock.ofType<OrganizationProvider>();
+    secondSubMock.object.name = 'secondsub';
+    secondSubMock.object.namespace = 'secondsub.realsub.testorg.iam.ewc';
+    const subsubMock = TypeMoq.Mock.ofType<OrganizationProvider>();
+    subsubMock.object.name = 'subsub';
+    subsubMock.object.namespace = 'subsub.testorg.iam.ewc';
+
+    const result = getOrgHierarchy([secondSubMock.object, subsubMock.object], secondSubMock.object);
     expect(result.length).toBe(1);
   });
 });
