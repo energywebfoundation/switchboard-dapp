8a723176e39895e0437c40728434b851c67b56e5
dawidgil <dawid.gil@j-labs.pl>
2022-02-20 23:17:26 +0100

    refactor: clean up notification container component

--- src/app/layout/header/notification-container/notification-container.component.ts
+++ src/app/layout/header/notification-container/notification-container.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification-container',
   templateUrl: './notification-container.component.html',
-  styleUrls: ['./notification-container.component.scss']
+  styleUrls: ['./notification-container.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NotificationContainerComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NotificationContainerComponent {}
