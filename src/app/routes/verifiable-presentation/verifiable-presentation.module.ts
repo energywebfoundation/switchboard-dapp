f70cdffe0f41ad7bf5415b318b77e43550ee010b
dawidgil <dawid.gil@j-labs.pl>
2022-03-07 15:29:00 +0100

    feat: create header component for VP

--- src/app/routes/verifiable-presentation/verifiable-presentation.module.ts
+++ src/app/routes/verifiable-presentation/verifiable-presentation.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VerifiablePresentationComponent } from './verifiable-presentation/verifiable-presentation.component';
 import { RouterModule } from '@angular/router';
+import { VpHeaderComponent } from './vp-header/vp-header.component';
 
 @NgModule({
-  declarations: [VerifiablePresentationComponent],
+  declarations: [VerifiablePresentationComponent, VpHeaderComponent],
   imports: [
     CommonModule,
     RouterModule.forChild([
