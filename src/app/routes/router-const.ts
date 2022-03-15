fb805219bc1471e8f4bf24567ca5280edf64676f
dawidgil <dawid.gil@j-labs.pl>
2022-03-15 13:21:04 +0100

    refactor: create router const object for handling navigation

---
+++ src/app/routes/router-const.ts
@@ -0,0 +1,9 @@
+export const RouterConst = {
+  Dashboard: 'dashboard',
+  Assets: 'assets',
+  Enrolment: 'enrolment',
+  Governance: 'governance',
+  SearchResult: 'search-result',
+  Enrol: 'enrol',
+  Welcome: 'welcome'
+}
