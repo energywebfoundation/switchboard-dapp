c42b7d39adf3922dd1cf36086b5c0e62ec0c7978
whitneypurdum <whitney.purdum@energyweb.org>
2022-06-09 11:56:07 +0100

    refactor: create MatTable in received pres component

--- src/app/routes/verifiable-presentation/received-presentations/received-presentations.component.ts
+++ src/app/routes/verifiable-presentation/received-presentations/received-presentations.component.ts
@@ -1,25 +1,27 @@
 import { Component, Input } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ICredentialTableData } from '../models/credential-table-data.interface';
+import { IVerifiableCredential} from '@sphereon/pex';
+import { MatSelect } from '@angular/material/select';
 
 @Component({
   selector: 'app-received-presentations',
   templateUrl: './received-presentations.component.html',
   styleUrls: ['./received-presentations.component.scss'],
 })
-export class ReceivedPresentationsComponent {
-  @Input() tableData; 
+export class ReceivedPresentationsComponent  {
+  tableDataFormatted: MatTableDataSource<ICredentialTableData>;
+  @Input() set tableData(data: ICredentialTableData[]) {
+    this.dataSource = new MatTableDataSource<ICredentialTableData>(data);
+  }
   @Input() submitDisabled: boolean;
-  @Input() credentialSelected
-  @Input() requiredCredentials
+  @Input() requiredCredentials: { [key: string]: IVerifiableCredential };
   displayedColumns: string[] = ['descriptor', 'verification'];
+  dataSource = new MatTableDataSource<unknown>([]);
 
-  handleCredentialUpdate(data: any) {
+
+  handleCredentialUpdate(data: MatSelect) {
+    console.log(data, "what is this?")
     this.requiredCredentials[data?.value?.descriptor] = data?.value?.credential;
    }
 }
-
-
-
-
-
-//http://localhost:4200/vp?_oob=eyJwcmVzZW50YXRpb25MaW5rIjp7InR5cGUiOiJzdHJpbmciLCJ1cmwiOiJzdHJpbmciLCJzc2lTZXNzaW9uIjoic3RyaW5nIn0sIm9jcGlUb2tlblVJRCI6IjVjODdhYzNlLTQwMGMtNDA2Yy05NjAwLTMyYzhmNzA1YmQyMiJ9
-// eyJwcmVzZW50YXRpb25MaW5rIjp7InR5cGUiOiJzdHJpbmciLCJ1cmwiOiJzdHJpbmciLCJzc2lTZXNzaW9uIjoic3RyaW5nIn0sIm9jcGlUb2tlblVJRCI6IjVjODdhYzNlLTQwMGMtNDA2Yy05NjAwLTMyYzhmNzA1YmQyMiJ9
