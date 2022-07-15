f88752e34713f48a9eb4d1e962945cbfd05f3a35
dawidgil <83639710+dawidgil@users.noreply.github.com>
2022-07-15 22:11:32 +0200

    feat: improve UI for setting validity period
    * feat: improve UI for setting validity period
    
    * refactor: use better naming for timestamp
    
    * test: more tests for validity period

--- src/app/routes/applications/new-role/components/validity-period/validity-period.component.spec.ts
+++ src/app/routes/applications/new-role/components/validity-period/validity-period.component.spec.ts
@@ -47,19 +47,28 @@ describe('ValidityPeriodComponent', () => {
     component.validityPeriod = 135;
     fixture.detectChanges();
 
-    const { periodInput } = getSelectors(hostDebug);
+    const { yearsInput, daysInput, hoursInput, minutesInput, secondsInput } =
+      getSelectors(hostDebug);
 
-    expect(periodInput.value).toEqual('135');
+    expect(+yearsInput.value).toEqual(0);
+    expect(+daysInput.value).toEqual(0);
+    expect(+hoursInput.value).toEqual(0);
+    expect(+minutesInput.value).toEqual(2);
+    expect(+secondsInput.value).toEqual(15);
   });
 
   it('should emit value from the input', () => {
     component.validityPeriod = undefined;
     fixture.detectChanges();
 
-    const { periodInput, periodNext } = getSelectors(hostDebug);
+    const { minutesInput, secondsInput, periodNext } = getSelectors(hostDebug);
 
-    periodInput.value = 100;
-    dispatchInputEvent(periodInput);
+    secondsInput.value = 40;
+    dispatchInputEvent(secondsInput);
+    fixture.detectChanges();
+
+    minutesInput.value = 1;
+    dispatchInputEvent(minutesInput);
     fixture.detectChanges();
 
     periodNext.click();
@@ -67,9 +76,42 @@ describe('ValidityPeriodComponent', () => {
 
     expect(nextSpy).toHaveBeenCalledWith(100);
   });
+
+  it('should return 10 minutes when passing object contains only minutes', () => {
+    fixture.detectChanges();
+
+    const { minutesInput, periodNext } = getSelectors(hostDebug);
+
+    minutesInput.value = 10;
+    dispatchInputEvent(minutesInput);
+    fixture.detectChanges();
+
+    periodNext.click();
+    fixture.detectChanges();
+
+    expect(nextSpy).toHaveBeenCalledWith(600);
+  });
+
+  it('should update inputs while containing only minutes', () => {
+    component.validityPeriod = 600;
+    fixture.detectChanges();
+
+    const { yearsInput, daysInput, hoursInput, minutesInput, secondsInput } =
+      getSelectors(hostDebug);
+
+    expect(+yearsInput.value).toEqual(0);
+    expect(+daysInput.value).toEqual(0);
+    expect(+hoursInput.value).toEqual(0);
+    expect(+minutesInput.value).toEqual(10);
+    expect(+secondsInput.value).toEqual(0);
+  });
 });
 
 const getSelectors = (hostDebug) => ({
-  periodInput: getElement(hostDebug)('period')?.nativeElement,
+  yearsInput: getElement(hostDebug)('years')?.nativeElement,
+  daysInput: getElement(hostDebug)('days')?.nativeElement,
+  hoursInput: getElement(hostDebug)('hours')?.nativeElement,
+  minutesInput: getElement(hostDebug)('minutes')?.nativeElement,
+  secondsInput: getElement(hostDebug)('seconds')?.nativeElement,
   periodNext: getElement(hostDebug)('period-next')?.nativeElement,
 });
