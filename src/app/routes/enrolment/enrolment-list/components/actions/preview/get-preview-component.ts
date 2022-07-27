ff0796eec1f2916945a12bb8506b4d14831a36f8
dawidgil <83639710+dawidgil@users.noreply.github.com>
2022-07-27 23:51:22 +0200

    fix: displaying preview from owned enrolments
    * fix: displaying preview from owned enrolments
    
    * test: add for checking displayed dialog

---
+++ src/app/routes/enrolment/enrolment-list/components/actions/preview/get-preview-component.ts
@@ -0,0 +1,8 @@
+import { EnrolmentListType } from '../../../models/enrolment-list-type.enum';
+import { IssuerRequestsComponent } from '../../../../view-requests/issuer-requests/issuer-requests.component';
+import { ViewRequestsComponent } from '../../../../view-requests/view-requests.component';
+
+export const GetPreviewComponent = new Map()
+  .set(EnrolmentListType.ISSUER, IssuerRequestsComponent)
+  .set(EnrolmentListType.ASSET, ViewRequestsComponent)
+  .set(EnrolmentListType.APPLICANT, ViewRequestsComponent);
--- src/app/routes/enrolment/enrolment-list/components/actions/preview/preview.component.spec.ts
+++ src/app/routes/enrolment/enrolment-list/components/actions/preview/preview.component.spec.ts
@@ -2,6 +2,12 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PreviewComponent } from './preview.component';
 import { MatDialog } from '@angular/material/dialog';
+import { EnrolmentListType } from '../../../models/enrolment-list-type.enum';
+import { getElement } from '@tests';
+import { IssuerRequestsComponent } from '../../../../view-requests/issuer-requests/issuer-requests.component';
+import { ViewRequestsComponent } from '../../../../view-requests/view-requests.component';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('PreviewComponent', () => {
   let component: PreviewComponent;
@@ -13,6 +19,7 @@ describe('PreviewComponent', () => {
       TestBed.configureTestingModule({
         declarations: [PreviewComponent],
         providers: [{ provide: MatDialog, useValue: matDialogSpy }],
+        schemas: [NO_ERRORS_SCHEMA],
       }).compileComponents();
     })
   );
@@ -20,10 +27,58 @@ describe('PreviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PreviewComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    matDialogSpy.open.and.returnValue({ afterClosed: () => of(true) });
   });
 
   it('should create', () => {
+    fixture.detectChanges();
+
     expect(component).toBeTruthy();
   });
+
+  it('should open IssuerRequests dialog', () => {
+    component.enrolmentType = EnrolmentListType.ISSUER;
+    fixture.detectChanges();
+
+    const { previewBtn } = getSelectors(fixture.debugElement);
+
+    previewBtn.click();
+
+    expect(matDialogSpy.open).toHaveBeenCalledWith(
+      IssuerRequestsComponent,
+      jasmine.objectContaining({})
+    );
+  });
+
+  it('should open ViewRequests dialog when type is applicant', () => {
+    component.enrolmentType = EnrolmentListType.APPLICANT;
+    fixture.detectChanges();
+
+    const { previewBtn } = getSelectors(fixture.debugElement);
+
+    previewBtn.click();
+
+    expect(matDialogSpy.open).toHaveBeenCalledWith(
+      ViewRequestsComponent,
+      jasmine.objectContaining({})
+    );
+  });
+
+  it('should open ViewRequests dialog when type is asset', () => {
+    component.enrolmentType = EnrolmentListType.ASSET;
+    fixture.detectChanges();
+
+    const { previewBtn } = getSelectors(fixture.debugElement);
+
+    previewBtn.click();
+
+    expect(matDialogSpy.open).toHaveBeenCalledWith(
+      ViewRequestsComponent,
+      jasmine.objectContaining({})
+    );
+  });
+});
+
+const getSelectors = (hostDebug) => ({
+  previewBtn: getElement(hostDebug)('preview')?.nativeElement,
 });
