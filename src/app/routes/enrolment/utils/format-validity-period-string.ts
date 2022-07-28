fe0715ca4eda80d63cb646f4a48c4e18e472e3a1
whitneypurdum <whitney.purdum@energyweb.org>
2022-07-27 20:14:51 -0400

    fix(exp): update formatter to use enums

--- src/app/routes/enrolment/utils/format-validity-period-string.ts
+++ src/app/routes/enrolment/utils/format-validity-period-string.ts
@@ -2,38 +2,56 @@ export const formatValidityPeriod = (num: number): string => {
   if (!num) {
     return null;
   }
-  const seconds = Math.floor(num % 60);
-  const minutes = Math.floor((num % 3600) / 60);
-  const hours = Math.floor((num % (3600 * 24)) / 3600);
-  const days = Math.floor(num / (3600 * 24));
 
-  const unitTimesFormatted = {
-    0: {
-      singular: 'day',
-      plural: 'days',
-    },
-    1: {
-      singular: 'hour',
-      plural: 'hours',
-    },
-    2: {
-      singular: 'minute',
-      plural: 'minutes',
-    },
-    3: {
-      singular: 'second',
-      plural: 'seconds',
-    },
+  enum UnitsOfTime {
+    YEAR = 'year',
+    YEARS = 'years',
+    DAY = 'day',
+    DAYS = 'days',
+    HOUR = 'hour',
+    HOURS = 'hours',
+    MINUTE = 'minute',
+    MINUTES = 'minutes',
+    SECOND = 'second',
+    SECONDS = 'seconds',
+  }
+
+  const years = {
+    value: Math.floor(num / (3600 * 24 * 365)),
+    singular: UnitsOfTime.YEAR,
+    plural: UnitsOfTime.YEARS,
+  };
+  const days = {
+    value: years.value
+      ? Math.floor(Math.floor(num / (3600 * 24)) / (365 * years.value))
+      : Math.floor(num / (3600 * 24)),
+    singular: UnitsOfTime.DAY,
+    plural: UnitsOfTime.DAYS,
   };
 
-  const unitsOfTime = [days, hours, minutes, seconds];
+  const hours = {
+    value: Math.floor((num % (3600 * 24)) / 3600),
+    singular: UnitsOfTime.HOUR,
+    plural: UnitsOfTime.HOURS,
+  };
+
+  const minutes = {
+    value: Math.floor((num % 3600) / 60),
+    singular: UnitsOfTime.MINUTE,
+    plural: UnitsOfTime.MINUTES,
+  };
+
+  const seconds = {
+    value: Math.floor(num % 60),
+    singular: UnitsOfTime.SECOND,
+    plural: UnitsOfTime.SECONDS,
+  };
+  const unitsOfTime = [years, days, hours, minutes, seconds];
   const timeFormatted = unitsOfTime
-    .map((val, i) => {
-      if (val && val !== 0) {
-        return `${val} ${
-          val > 1
-            ? unitTimesFormatted[i]['plural']
-            : unitTimesFormatted[i]['singular']
+    .map((timePeriod) => {
+      if (timePeriod.value && timePeriod.value !== 0) {
+        return `${timePeriod.value} ${
+          timePeriod.value > 1 ? timePeriod.plural : timePeriod.singular
         }`;
       }
     })
