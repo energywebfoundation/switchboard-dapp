41e32341f8a18ef1b08cbdd692ae7e75ecdb2b0c
dawidgil <dawid.gil@j-labs.pl>
2022-08-10 14:13:41 +0200

    fix: issue with shared user data between accounts

--- src/app/layout/header/dialog-user/dialog-user.component.ts
+++ src/app/layout/header/dialog-user/dialog-user.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
-import * as userSelectors from '../../../state/user-claim/user.selectors';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
-import * as userActions from '../../../state/user-claim/user.actions';
 import { deepEqualObjects } from '@utils';
-
-const MAJORITY_AGE = 18;
+import { UserClaimActions, UserClaimSelectors } from '@state';
 
 @Component({
   selector: 'app-dialog-user',
@@ -17,11 +14,8 @@ const MAJORITY_AGE = 18;
 export class DialogUserComponent implements OnInit, OnDestroy {
   public profileForm: FormGroup = this.fb.group({
     name: ['', [Validators.maxLength(256), Validators.required]],
-    birthdate: ['', Validators.required],
-    address: ['', [Validators.maxLength(500), Validators.required]],
   });
   private defaultFormValues;
-  public maxDate: Date;
   private destroy$ = new Subject<void>();
 
   constructor(private fb: FormBuilder, private store: Store) {}
@@ -32,15 +26,12 @@ export class DialogUserComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    this.maxDate = this.getLegalAge();
     this.store
-      .select(userSelectors.getUserData)
+      .select(UserClaimSelectors.getUserData)
       .pipe(takeUntil(this.destroy$))
       .subscribe((profile) => {
         this.profileForm.patchValue({
           name: profile?.name,
-          birthdate: new Date(profile?.birthdate),
-          address: profile?.address,
         });
         this.defaultFormValues = this.profileForm.value;
       });
@@ -59,7 +50,7 @@ export class DialogUserComponent implements OnInit, OnDestroy {
       return;
     }
     this.store.dispatch(
-      userActions.updateUserData({ userData: this.getProfile() })
+      UserClaimActions.updateUserData({ userData: this.getProfile() })
     );
   }
 
@@ -67,13 +58,6 @@ export class DialogUserComponent implements OnInit, OnDestroy {
     const profileData = this.profileForm.getRawValue();
     return {
       ...profileData,
-      birthdate: profileData.birthdate.getTime(),
     };
-  }
-
-  private getLegalAge() {
-    const today = new Date();
-    today.setFullYear(today.getFullYear() - MAJORITY_AGE);
-    return today;
   }
 }
