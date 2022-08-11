66e598ecbf647145de7a310aec84419c6acd06b0
dawidgil <dawid.gil@j-labs.pl>
2022-08-11 13:09:36 +0200

    refactor: remove enrol to role component

--- src/app/routes/enrolment/enrol-to-role/enrol-to-role.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-enrol-to-role',
-  templateUrl: './enrol-to-role.component.html',
-  styleUrls: ['./enrol-to-role.component.scss'],
-})
-export class EnrolToRoleComponent {}
