78cc0f37f12294da546d8c3d0bab8e0907d0babe
whitneypurdum <whitney.purdum@energyweb.org>
2022-11-01 16:15:13 -0400

    test: add tests for button text

--- src/app/routes/applications/applications.component.spec.ts
+++ src/app/routes/applications/applications.component.spec.ts
@@ -1,24 +1,78 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DialogService } from 'src/app/shared/services/dialog/dialog.service';
+import { EnvService } from 'src/app/shared/services/env/env.service';
+import { IamService } from 'src/app/shared/services/iam.service';
+import { UrlParamService } from 'src/app/shared/services/url-param.service';
+import { provideMockStore } from '@ngrx/store/testing';
+import { MockActivatedRoute, dialogSpy } from '@tests';
 import { ApplicationsComponent } from './applications.component';
+import { DomainsService } from 'iam-client-lib';
 
-xdescribe('ApplicationsComponent', () => {
+describe('ApplicationsComponent', () => {
   let component: ApplicationsComponent;
   let fixture: ComponentFixture<ApplicationsComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ApplicationsComponent],
-    }).compileComponents();
+  let dialog;
+  let domainsService;
+  const routerSpy = jasmine.createSpyObj('Router', [
+    'navigateByUrl',
+    'navigate',
+  ]);
+  let activatedRouteStub: MockActivatedRoute;
+  describe('NON-PRODUCTION BEHAVIOR', () => {
+    beforeEach(() => {
+      dialog = jasmine.createSpyObj(DialogService, ['open']);
+      activatedRouteStub = new MockActivatedRoute();
+      domainsService = jasmine.createSpyObj('domainsService', ['isOwner']);
+      TestBed.configureTestingModule({
+        declarations: [ApplicationsComponent],
+        providers: [
+          { provide: MatDialog, useValue: dialogSpy },
+          { provide: EnvService, useValue: { production: false } },
+          { provide: IamService, useValue: { domainsService: domainsService } },
+          { provide: UrlParamService, useValue: {} },
+          { provide: Router, useValue: routerSpy },
+          { provide: ActivatedRoute, useValue: activatedRouteStub },
+          provideMockStore(),
+        ],
+      }).compileComponents();
+      fixture = TestBed.createComponent(ApplicationsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      domainsService.isOwner.and.returnValue(Promise.resolve(true));
+    });
+    it('should create Request Organization Button text correctly based on env (non-production)', () => {
+      expect(component.orgRequestButtonText).toBe('Create Organization');
+    });
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ApplicationsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('PRODUCTION BEHAVIOR', () => {
+    beforeEach(() => {
+      dialog = jasmine.createSpyObj(DialogService, ['open']);
+      activatedRouteStub = new MockActivatedRoute();
+      domainsService = jasmine.createSpyObj('domainsService', ['isOwner']);
+      TestBed.configureTestingModule({
+        declarations: [ApplicationsComponent],
+        providers: [
+          { provide: MatDialog, useValue: dialogSpy },
+          { provide: EnvService, useValue: { production: true } },
+          { provide: IamService, useValue: { domainsService: domainsService } },
+          { provide: UrlParamService, useValue: {} },
+          { provide: Router, useValue: routerSpy },
+          { provide: ActivatedRoute, useValue: activatedRouteStub },
+          provideMockStore(),
+        ],
+      }).compileComponents();
+      fixture = TestBed.createComponent(ApplicationsComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      domainsService.isOwner.and.returnValue(Promise.resolve(true));
+    });
+    it('should create Request Organization Button text correctly based on env (production)', () => {
+      expect(component.orgRequestButtonText).toBe(
+        'Request to Create Organization'
+      );
+    });
   });
 });
