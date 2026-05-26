30e5d3d8fc2c33cfa9de4dcf379f1069db442124
Dani <danielserrano.contacto@gmail.com>
2026-05-26 12:04:54 +0200

    chore: notifications websocket not blocking rendering

--- src/app/shared/services/message-subscription/message-subscription.service.spec.ts
+++ src/app/shared/services/message-subscription/message-subscription.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { fakeAsync, TestBed, tick } from '@angular/core/testing';
 
 import { MessageSubscriptionService } from './message-subscription.service';
 import { IamService } from '../iam.service';
@@ -35,4 +35,54 @@ describe('MessageSubscriptionService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should initialize the message subscription without waiting for it to resolve', fakeAsync(() => {
+    let resolveSubscription: (subscriptionId: number) => void = () => undefined;
+    messagingServiceSpy.subscribeTo.and.returnValue(
+      new Promise((resolve) => {
+        resolveSubscription = resolve;
+      }) as any
+    );
+
+    service.init();
+
+    expect(messagingServiceSpy.subscribeTo).toHaveBeenCalledTimes(1);
+
+    service.init();
+
+    expect(messagingServiceSpy.subscribeTo).toHaveBeenCalledTimes(1);
+
+    resolveSubscription(1);
+    tick();
+
+    service.init();
+
+    expect(messagingServiceSpy.subscribeTo).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should not throw when the message subscription fails', fakeAsync(() => {
+    spyOn(console, 'error');
+    messagingServiceSpy.subscribeTo.and.returnValue(
+      Promise.reject(new Error('connection failed')) as any
+    );
+
+    expect(() => service.init()).not.toThrow();
+    tick();
+
+    expect(console.error).toHaveBeenCalled();
+  }));
+
+  it('should only unsubscribe when a subscription exists', fakeAsync(() => {
+    messagingServiceSpy.subscribeTo.and.returnValue(Promise.resolve(1) as any);
+
+    service.ngOnDestroy();
+
+    expect(messagingServiceSpy.unsubscribeFrom).not.toHaveBeenCalled();
+
+    service.init();
+    tick();
+    service.ngOnDestroy();
+
+    expect(messagingServiceSpy.unsubscribeFrom).toHaveBeenCalledOnceWith(1);
+  }));
 });
